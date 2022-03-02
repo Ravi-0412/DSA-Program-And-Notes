266a3c36e84b32a6de006e4c3fdd7425d6aaea00
Ravi-0412 <ravikr1035@gmail.com>
2022-03-03 02:39:20 +0530

    recursion fibonacii

--- recursion/intro.py
+++ recursion/intro.py
@@ -23,9 +23,32 @@
 
 
 # recursive way 
-def show(n):
-    if n>5:   # base condition
-        return
-    print(n)
-    show(n+1)
-show(1)
+# def show(n):
+#     if n>5:   # base condition: where the recursion will stop
+#         return  # no base condition will give "stackoverflow error["
+#     print(n)
+#     show(n+1)
+# show(1)
+
+
+# find nth fibonacii number
+# def fibonacii(n):
+#     if n==0 or n==1:
+#         return n
+#     else:
+#         return fibonacii(n-1) + fibonacii(n-2)
+
+# print(fibonacii(9))
+
+
+# fibonacii using DP
+def fibonacii(n):
+    fib= [0,1]   # base case value are stored initially in the array
+    for i in range(2, n+1):
+        fib.append(fib[i-1] + fib[i-2])
+        print(fib[i])
+    return fib[n]
+
+print(fibonacii(9))
+
+

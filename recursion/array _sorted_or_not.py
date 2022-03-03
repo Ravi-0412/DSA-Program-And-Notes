f1dc3cb5711485b2ef5d5b74beaeb64ee9c52106
Ravi-0412 <ravikr1035@gmail.com>
2022-03-04 00:44:15 +0530

    03/03/2022 work

---
+++ recursion/array _sorted_or_not.py
@@ -0,0 +1,48 @@
+# method 1: just check if any element on right side 
+# is greater than the ele on its lfet side
+# submitted on GFG
+class Solution:
+    def arraySortedOrNot(self, arr, n):
+        for i in range(n-1):
+            if arr[i]>arr[i+1]:
+                return False
+        return True
+
+
+# By Recursion(better one for recursion)
+def sortedOrNot(arr,start,end):
+    while(start<end):   
+        if arr[start]> arr[start+1]:
+            return False
+        else:
+            return sortedOrNot(arr,start+1,end)
+    return True
+
+# arr= [2,4,5,6,7,8]
+arr= [1,5,3,9,10,54]
+print(sortedOrNot(arr,0,len(arr)-2))
+
+
+# another way of writing recursion function
+# def sortedOrNot(arr,start,end):
+#     while(start<end):   
+#         if arr[start]> arr[start+1]:
+#             return False
+#         else:
+#             return (arr[start+1]<=arr[start+2]) and sortedOrNot(arr,start+2,end)
+# arr= [2,4,4,5,6,7,8]
+# # arr= [1,5,3,9,10,54]
+# print(sortedOrNot(arr,0,len(arr)-2))
+
+
+#another way of writing recursion function(better one)
+def sortedOrNot(arr, index):
+    if index== len(arr)-1:  # if index is the index of last ele of the array then True
+                            # as last ele will be always sorted
+        return True   
+    else: # check the value of next ele and call the function for next index
+        return (arr[index]<=arr[index+1]) and sortedOrNot(arr,index+1)
+# arr= [2,4,4,5,6,7,8]
+arr= [1,5,3,9,10,54]
+print(sortedOrNot(arr,0))
+
--- recursion/intro.py
+++ recursion/intro.py
@@ -42,13 +42,139 @@
 
 
 # fibonacii using DP
-def fibonacii(n):
-    fib= [0,1]   # base case value are stored initially in the array
-    for i in range(2, n+1):
-        fib.append(fib[i-1] + fib[i-2])
-        print(fib[i])
-    return fib[n]
+# def fibonacii(n):
+#     fib= [0,1]   # base case value are stored initially in the array
+#     for i in range(2, n+1):
+#         fib.append(fib[i-1] + fib[i-2])
+#     return fib[n]
 
-print(fibonacii(9))
+# print(fibonacii(9))
+
+
+# binary search using recusrsion
+# def BinarySearch(arr,key,start,end):
+#     mid= start + (end-start)//2
+#     while(start<=end):
+#         if arr[mid]== key:
+#             return mid
+#         elif arr[mid]>key:
+#             return BinarySearch(arr,key,start,mid-1)
+#         else:
+#             return BinarySearch(arr,key,mid+1,end)
+#     return -1
+
+# arr= [2,4,5,7,9,10,15,22]
+# print(BinarySearch(arr,5,0,len(arr)-1))
+
+
+# sum of 1st n natural number: By recursion
+# def sum(n):
+#     if n==1:
+#         return 1
+#     else:
+#         return n + sum(n-1)
+
+# print(sum(5))
+
+
+#linear search using Recursion  # will not work if ele is repeated
+# def LinearSearch(arr,target,index):  # index tells from where we have to search
+#     if index== len(arr): # means ele is not present as we have traversed the whole array
+#         return -1
+#     elif arr[index]== target:
+#         return index
+#     else:
+#         return LinearSearch(arr,target,index+1)
+    
+# arr= [2,4,5,7,9,10,15,22]
+# print(LinearSearch(arr,15,0))
+
+
+# in binary search return a list containing all the index of the target ele
+# indexes= []
+# def LinearSearch(arr,target,index):  # index tells from where we have to search
+#     if index== len(arr): # means ele is not present as we have traversed the whole array
+#         pass
+#     elif arr[index]== target: # if ele is found add in the list and again seasrch for further indexes
+#         indexes.append(index)  
+#         LinearSearch(arr,target,index+1)
+#     else:
+#         LinearSearch(arr,target,index+1)
+#     if indexes==[]: # if ele is not present
+#         return -1
+#     else:
+#         return indexes
+    
+# arr= [2,4,5,7,4,9,4,10,15,22,4]
+# print(LinearSearch(arr,4,0))
+# # print(LinearSearch(arr,18,0))
+
+
+# Q: in binary search return a list containing all the index of the target ele
+# method2(better one) if you dont want to create a list outside function to store the indices
+# in this you have to pass the index array as parameter
+# variable passed as parameter has scope in all the function calls
+# and variable declared inside the function has scope only within that function
+
+# def LinearSearch(arr,target,index,list1):  # index tells from where we have to search
+#     if index== len(arr): # as we have traversed the whole array
+#         return list1
+#     if arr[index]== target: # if ele is found add in the list and again seasrch for further indexes
+#         list1.append(index) 
+#     # LinearSearch(arr,target,index+1,list1) # this will print 'None' as we are not returning
+#                                                     # anything back to the previous called function
+#     return LinearSearch(arr,target,index+1,list1) # search for next indices   
+# arr= [2,4,5,7,4,9,4,10,15,22,4]
+# indexes= []
+# print(LinearSearch(arr,4,0,indexes))
+# print(LinearSearch(arr,18,0,indexes))
+
+
+# other method of above Q
+# def LinearSearch(arr,target,index,list1):  # index tells from where we have to search
+#     if index== len(arr): # as we have traversed the whole array 
+#         pass
+#     elif arr[index]== target: # if ele is found add in the list and again seasrch for further indexes
+#         list1.append(index)  
+#         LinearSearch(arr,target,index+1,list1) # search for next indices 
+#     else:
+#         LinearSearch(arr,target,index+1,list1) # search for next indices 
+#     return list1
+
+# arr= [2,4,5,7,4,9,4,10,15,22,4]
+# indexes= []
+# # print(LinearSearch(arr,4,0,indexes))
+# print(LinearSearch(arr,18,0,indexes))
+
+
+#Do the same above Q but dont take list as argument, make list inside the function body itself
+# just for knowledge and concept
+def LinearSearch(arr,target,index):  # index tells from where we have to search
+    list1= []
+    if index== len(arr): # as we have traversed the whole array 
+        pass
+    elif arr[index]== target: # if ele is found add the index into the list and also
+                              # add the list returned by the further function calls
+        list1.append(index)  
+        c= LinearSearch(arr,target,index+1)  # store the list returned to add with the previous list 
+        list1= list1+ c   # will comtain all the index of target element till now
+
+        # list1.append(index)      # writing this and next onen below lines as combination not giving 
+        #                         #correct output 
+        # list1+ LinearSearch(arr,target,index+1)  # list returned by the next function is not getting added
+                                                # as after returning the value it will execute the further lines
+                                                # or any statement ahead and we are adding in the same line when when we are calling
+
+        # list1.append()+ LinearSearch(arr,target,index+1)   # writing only this giving None(None+list)
+
+    else:
+        return LinearSearch(arr,target,index+1)
+    return list1
+
+arr= [2,4,5,7,4,9,4,10,15,22,4]
+print(LinearSearch(arr,4,0))
+# print(LinearSearch(arr,18,0))
+
+
 
 

de99bb1d86ec90aad7a7d9836cdd84f4b72b802a
Ravi-0412 <ravikr1035@gmail.com>
2022-09-20 09:54:00 +0530

    till 20/09/2022

--- Bitwise_Manipulation/set_kth_bit.py
+++ Bitwise_Manipulation/set_kth_bit.py
@@ -1,4 +1,5 @@
-# for setting we always use 'OR' with '1'
+# for setting we always use 'OR' with '1' at that position i.e keep 1 at that position and 
+# keep '0' at all other position i.e or the given number by 2^k
 # and for reset we use 'AND' with '0'
 # for getting one at kth position do left shift '1'
 # 'k' time(if last bit start from zero)

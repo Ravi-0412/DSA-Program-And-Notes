23266f6cfe6129f972b6683f282871bbaac6ab04
Ravi-0412 <ravikr1035@gmail.com>
2023-01-12 12:51:46 +0530

    12/01/2023

---
+++ Greedy/678. Valid Parenthesis String.py
@@ -0,0 +1,113 @@
+# checking valid paranthesis if it contains only two types of string i.e: '(' and ')'.
+
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        openCount= 0  # will count the no of open paranthesis
+        # it tells the exact no of ')' must be there for a valid string.
+        for i  in range(len(s)):
+            if s[i]== '(':
+                openCount+= 1
+            else:
+                openCount-= 1
+            if openCount < 0:   # '(' is less than ')'.
+                return False
+        return openCount== 0
+
+
+# actual Q
+# method 1: by Recursion(TLE)
+# time: O(3^n)
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        openCount= 0  # count the no of open paranthesis
+        return self.check(s, 0, openCount)  # '0': start index from where we have to check.
+    
+    def check(self, s, ind, openCount):
+        if openCount< 0:
+            return False
+        if ind== len(s):
+            return openCount== 0
+        if s[ind]== '(':
+            return self.check(s, ind+1, openCount +1)
+        elif s[ind]== ')':
+            if openCount <=0:
+                return False
+            return self.check(s, ind+1, openCount -1)
+        elif s[ind]== '*':  # we have three choices either treat this as 1) '('  2) ')' or 3) empty string. 
+                          # and if anu of them return True then return True.
+            return self.check(s, ind+1, openCount +1) or self.check(s, ind+1, openCount -1) or self.check(s, ind+1, openCount)
+
+# OR 
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        openCount= 0  # count the no of open paranthesis
+        return self.check(s, 0, openCount)  # '0': start index from where we have to check.
+    
+    def check(self, s, ind, openCount):
+        if openCount< 0:
+            return False
+        if ind== len(s):
+            return openCount== 0
+        if s[ind]== '(':
+            openCount+= 1
+        elif s[ind]== ')':
+            if openCount <=0: return False
+            openCount-= 1
+        elif s[ind]== '*':  # we have three choices either treat this as 1) '('  2) ')' or 3) empty string. 
+                            # and if anu of them return True then return True.
+            return self.check(s, ind+1, openCount +1) or self.check(s, ind+1, openCount -1) or self.check(s, ind+1, openCount)
+        return self.check(s, ind+1, openCount)    # if only either '(' or ')' comes at current index.
+
+# optimising the above solution
+# time= space= O(n)
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        openCount= 0  # count the no of open paranthesis
+        dp= [[-1 for i in range(len(s) +1)]for i in range(len(s) +1)]
+        return self.check(s, 0, openCount, dp)  # '0': start index from where we have to check.
+    
+    def check(self, s, ind, openCount, dp):
+        if openCount< 0:
+            return False
+        if ind== len(s):
+            return openCount== 0
+        if dp[ind][openCount] != -1:
+            return dp[ind][openCount]
+        if s[ind]== '(':
+            dp[ind][openCount]= self.check(s, ind+1, openCount +1, dp)
+        elif s[ind]== ')':
+            if openCount <=0:
+                return False
+            dp[ind][openCount]= self.check(s, ind+1, openCount -1, dp)
+        elif s[ind]== '*':  # we have three choices either treat this as 1) '('  2) ')' or 3) empty string. 
+                          # and if anu of them return True then return True.
+            dp[ind][openCount]= self.check(s, ind+1, openCount +1, dp) or self.check(s, ind+1, openCount -1, dp) or self.check(s, ind+1, openCount, dp)
+        return dp[ind][openCount]
+    
+
+# METHOD 2: optimising the above one to O(n)
+# time= O(n), space= O(1)
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        openMin, openMax= 0, 0   # max and min no of ')' that can be accomodated.
+        for i in range(len(s)):
+            if s[i]== '(':
+                openMin+= 1
+                openMax+= 1
+            elif s[i]== ')':
+                openMin-= 1
+                openMax-= 1
+            elif s[i]== '*':
+                openMin-= 1    # 'if '*' behaves as ')'. means one '(' is accomodated by '*'.
+                openMax+= 1    # 'if '*' behaves as '('. means one more matching of '(' is increased.
+
+            if openMax < 0:
+                return False
+            openMin= max(openMin, 0)    # openMin can't be negative.
+        return openMin== 0    # we are not waiting for anymore ')'.
+
+
+# Note: read solutions in sheet and also few comments under that.
+
+
+
---
+++ Greedy/763. Partition Labels.py
@@ -0,0 +1,37 @@
+# time= space= O(n)
+
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        n= len(s)
+        # form a hashmap which will store the last index of each char.
+        last_index = {s[i]: i for i in range(n)}   # s[i]: key ans 'i': value
+        start, ans= 0, []    # 'i' denotes the start of the partition
+        while start < n:
+            # minimum length will be equal to last index of 'start'.
+            # but it can go beyond also if char before 'last_index[s[start]]' have last occurence beyond 'last_index[s[start]]'
+            end, j= last_index[s[start]], start +1
+            while j < end:
+                if last_index[s[j]] > end:
+                    end= last_index[s[j]]
+                j+= 1
+            # now we have go the 1st partition. end will denote the last index of partition.
+            ans.append(end- start +1)
+            start= end +1   # now we will search for next partition from this index
+        return ans
+
+
+
+# checking valid paranthesis if it contains only two types of string i.e: '(' and ')'.
+
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        openCount= 0  # will count the no of open paranthesis
+        # it tells the exact no of ')' must be there for a valid string.
+        for i  in range(len(s)):
+            if s[i]== '(':
+                openCount+= 1
+            else:
+                openCount-= 1
+            if openCount < 0:   # '(' is less than ')'.
+                return False
+        return openCount== 0

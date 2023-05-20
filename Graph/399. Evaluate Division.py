2cd89a9f71678ded9b8f94b3912715e6412a8525
Ravi-0412 <ravikr1035@gmail.com>
2023-05-20 19:59:48 +0530

    20/05/2023

---
+++ Graph/399. Evaluate Division.py
@@ -0,0 +1,56 @@
+# logic: just simple bfs to find the cost from src to destination.
+# Difficult part to think how we can do by graph.
+
+# expalanation:
+# Given:
+# a/b = 2.0, b/c = 3.0
+# We can build a directed graph:
+# a -- 2.0 --> b -- 3.0 --> c
+# If we were asked to find a/c, we have:
+# a/c = a/b * b/c = 2.0 * 3.0
+# In the graph, it is the product of costs of edges.
+
+# Do notice that, 2 edges need to added into the graph with one given equation,
+# because with a/b we also get result of b/a, which is the reciprocal of a/b.
+
+# so the previous example also gives edges:
+# c -- 0.333 --> b -- 0.5 --> a
+
+
+# time: n * (E+ V), n= len(queries)
+
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        adj= collections.defaultdict(list)
+        # form adjacency list (directed graph with values for that eqn)
+        for i, eq in enumerate(equations):
+            a, b= eq
+            adj[a].append((b, values[i]))
+            adj[b].append((a, 1 / values[i]))  # for reverse value will also reverse
+        
+        def bfs(src, target):
+            if src not in adj or target not in adj:
+                return -1
+            q= collections.deque()
+            visited= set()
+            q.append((src, 1))   # [variable, values_till_now]
+            visited.add(src)
+            while q:
+                a, w= q.popleft()
+                if a== target:
+                    return w
+                for nei, weight in adj[a]:
+                    if nei not in visited:
+                        q.append((nei, w *weight))
+                        visited.add(nei)
+            # Both src and destination is present but there is no way we can find src/target.
+            return -1
+        
+        ans= []
+        for q in queries:
+            curAns= bfs(q[0], q[1])
+            ans.append(curAns)
+        return ans
+
+
+# Later do by dfs

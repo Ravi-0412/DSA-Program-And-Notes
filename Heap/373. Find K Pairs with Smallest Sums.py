1e77b070f869e663c7f1b454a60e355717a1c8ad
Ravi <ravikr1035@gmail.com>
2023-06-27 22:05:08 +0530

    27/06/2023

--- Heap/373. Find K Pairs with Smallest Sums.py
+++ Heap/373. Find K Pairs with Smallest Sums.py
@@ -1,6 +1,11 @@
-# smallest ele will at the start only but can form from any combination.
-# for any ele we will pop there are two possibility and while incr the pointers we can get the duplicates also.
-# like we can get same value as (i, j+1) from (i+1, j)  and vice versa also. so we will get wrong ans.
+# smallest ele will at the start only so at start we will push in heap :((nums1[0]+ nums2[0],0,0)).
+
+# When we will pop any ele say at index (i, j) then next minimum we will get at (i+1, j) or (j+1, i) only.
+# Since there are two possibility we can get same index from other way also.
+# e.g: (2,2) -> (1,2) or (2, 1) so we will use visited set to keop track of visited indices.
+
+# 
+
 #  so we are marking the ele already visited
 # time: O(k*log(m+n))
 
@@ -10,7 +15,7 @@ class Solution:
         visited= set()
         visited.add((0,0))
         heap, ans= [], []
-        heapq.heappush(heap,(nums1[0]+ nums2[0],0,0))
+        heapq.heappush(heap,(nums1[0]+ nums2[0],0,0))   # (sum, i, j)  # i, j index of nums1 and nums2.
 
         while len(ans) < k and heap:
             sum,i,j= heapq.heappop(heap)

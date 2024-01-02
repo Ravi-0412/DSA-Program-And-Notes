3d66e6899caa005bb31a6a118c301d7f2a2cab58
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2024-01-02 13:34:35 +0530

    Update 621. Task Scheduler.py

--- Heap/621. Task Scheduler.py
+++ Heap/621. Task Scheduler.py
@@ -1,13 +1,17 @@
-# you just have to minimise the idle time and for this
-# process the most frequent one first as this will give more time to not be idle because
-# if you process the less frequent char first then at last a lot of most frequent char will left and you have to wait.
+# Logic: We have to minimise the idle time for overall minimum time.
+# And for this we need to utilise the cooldown time between two task.
+# And for reducing cooldown time between tasks process the most frequent one first as 
+# during cooldown time of most frequent task we can schedule other tasks. => utilising cooldwon period thus reducing idle time.
+
+# Note: if you process the less frequent char first then at last a lot of most frequent char will left and you have to wait increasing idle.
+
 # So from here we can think of using maxheap based on freq of each char
 
 # most frequent task to ek bar pick karne pe uskla count decr karke phir se add karna hoga next time most freq find karne ke liye
-# kyonki picked one ka freq count decr karne pe may ab most freq nhi rah payega
+# kyonki picked one ka freq count decr karne pe may be ab most freq nhi rah payega
 
-# when a task has been processed at time 't' then next time it will be processed at time 't+n'
-# to check if any taks is avaible to process at any time 't' we will take help of 'Queue'
+# when a task has been processed at time 't' then next time it will be processed at time 't+n'.
+# To check if any taks is available to process at any time 't' we will take help of 'Queue'
 
 # here only count of each letter(task) will matter not their name
 
@@ -27,7 +31,8 @@ class Solution:
         # then it will be counted as idle
         while maxHeap or q:  
             time+= 1
-            if maxHeap:  # we are basically processing the most freq task available at this time 
+            if maxHeap:  
+                # Process the most freq task available at this time 
                 cnt= 1+ heapq.heappop(maxHeap)   # basically decr the freq of current task(made max heap with negative value so adding '1')
                 if cnt!= 0:  # means same task has more time to process so add in 'Q' with [count,next_process_time]
                     q.append((cnt, time+ n))   # this task can be processed next time at 'time+n'

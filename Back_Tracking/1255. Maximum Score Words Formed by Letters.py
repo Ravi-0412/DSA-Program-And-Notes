a5abc1613ea8f638bda8c79aed9f95012a7b6bfc
Ravi <ravikr1035@gmail.com>
2024-05-25 08:35:30 +0530

    25/05/2024

---
+++ Back_Tracking/1255. Maximum Score Words Formed by Letters.py
@@ -0,0 +1,68 @@
+# Method 1:
+# Logic : 1) Just find all subsets of words
+# 2) check each subset whether valid or not.
+# a) for this frequency of char used in subset should <= frequency of characters in letters.
+# 3) if valid then return the score of each character in susbets
+
+# time: O(2**w * w * l). w = len(words),l = len(each word)
+
+class Solution:
+    def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
+        freq_letters = Counter(letters)
+        self.ans = 0
+        n = len(words)
+
+        def findScore(subset):
+            freq = collections.defaultdict(int)
+            sc = 0
+            for w in subset:
+                for c in w:
+                    freq[c] += 1
+                    if freq[c] > freq_letters[c]:
+                        return 0
+                    sc += score[ord(c) - ord('a')]
+            return sc
+                    
+        def backtrack(i, subset):
+            if i == n:
+                self.ans = max(self.ans, findScore(subset))  # time: O(w * l)
+                return
+            backtrack(i +1, subset)
+            backtrack(i + 1, subset + [words[i]])
+
+        backtrack(0, [])
+        return self.ans
+
+# Method 2:
+# better check the valid one before adding word[i].
+
+class Solution:
+    def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
+        if not words or not letters or not score:
+            return 0
+        
+        # Convert the letters list into a count of each letter
+        count = [0] * 26
+        for ch in letters:
+            count[ord(ch) - ord('a')] += 1
+        
+        def backtrack(words, count, score, index):
+            max_score = 0
+            for i in range(index, len(words)):
+                res = 0
+                is_valid = True
+                # Temporary decrease the count for the current word
+                for ch in words[i]:
+                    count[ord(ch) - ord('a')] -= 1
+                    res += score[ord(ch) - ord('a')]
+                    if count[ord(ch) - ord('a')] < 0:
+                        is_valid = False
+                if is_valid:
+                    res += backtrack(words, count, score, i + 1)
+                    max_score = max(res, max_score)
+                # Restore the count after trying the current word
+                for ch in words[i]:
+                    count[ord(ch) - ord('a')] += 1
+            return max_score
+        
+        return backtrack(words, count, score, 0)
--- Back_Tracking/140. Word Break II.py
+++ Back_Tracking/140. Word Break II.py
@@ -1,59 +1,32 @@
 # logic: just check which substring starting from start is present in 'dict, 
-# if it is present in dic then check for remaining string recursively and keep adding that to ans.
+# if it is present in dict then check for remaining string recursively and keep adding that to ans.
 
-# Time Complexity: O(2^n). Because there are 2^n combinations in worst case i.e when every char is present in the dictiionary
+# Time Complexity: O(2^n). Because there are 2^n combinations in worst case i.e when every char is present in the dictionary
 # in this case for every char we will have two options either to chose that char or not
 # space: O(n) recursive depth + O(n) for storing ans= O(n)
 
 # Cross Q / follow up 
 # NOte vvvi: "the same word in the dictionary may be reused multiple times in the segmentation.".
-# Due to this only we are able to check if cur substring of 's' matches with any word in 'dict'.
+# Due to this only we are able to check if cur substring of 's[i: j+ 1]' matches with any word in 'dict'.
 # If same word of dict is not allowed mutiple times then, we will have to take visited set for dict words(index will work)
 # to check whether the word at that index is already used or not.
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        ans= []  # will store the final ans
-        seg= []  # will store the matching segment
-        self.helper(s, wordDict,seg,ans)
-        return ans
-    
-    def helper(self,s,dict,seg, ans):
-        if not s:
-            # listToStr = reduce(lambda a, b : a+ " " +str(b), seg) # shorter way. 
-            # listToStr= " ".join(str(e) for e in seg)   # or do like this
-            listToStr = " ".join(seg)   # simpler and best
-            ans.append(listToStr)
-            return
-        for i in range(1,len(s)+1):
-            if s[:i] in dict:
-                # if present in dict then, search for remaining 's'
-                seg.append(s[:i])
-                self.helper(s[i:],dict,seg,ans)
-                # now backtrack so that it removes the current mapped string and found the new one
-                seg.pop()
+        n = len(s)
+        dictSet = set(wordDict)
+        ans = []
 
-
-# Better one
-class Solution:
-    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-
-        def helper(s, seg):
-            if not s:
-                ans.append(seg[: -1])   # removing the extra space at last
+        def backtrack(i, sent):
+            if i == n:
+                ans.append(" ".join(sent))
                 return
-            for i in range(1,len(s)+1):
-                if s[:i] in wordsSet:
-                    # if present in dict then, search for remaining 's'
-                    helper(s[i:] , seg + s[: i] + " ")
-
-
-        ans= []  # will store the final ans
-        wordsSet = set(wordDict)
-        helper(s, "")
+            for j in range(i, n):
+                if s[i: j + 1] in dictSet:
+                    backtrack(j + 1, sent + [s[i: j + 1]])
+        backtrack(0, [])
         return ans
 
 # Similar Questions:
 # 1) "139. Word Break"
-# https://github.com/Ravi-0412/DSA-Program-And-Notes/blob/main/Dynamic%20Programming/Matrix_Chain_Multiplication/Front%20Partitioning/139.%20Word%20Break.py
 

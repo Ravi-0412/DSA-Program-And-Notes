6aaa9e94fcaf3fb598c384aa3c4a6fb83d3d1e90
Ravi-0412 <ravikr1035@gmail.com>
2024-08-20 23:00:45 +0530

    20/08/2024

--- Dynamic Programming/Palindromic Subsequences/5. Longest Palindromic Substring.py
+++ Dynamic Programming/Palindromic Subsequences/5. Longest Palindromic Substring.py
@@ -1,4 +1,9 @@
-# thought to do same method as 'printing lcs' by was becoming very tough and not getting how to do.
+# Note: thought to do by same method as 'printing lcs' but was becoming very tough and not getting how to do.
+
+# method 1: Try all substring
+# Time: O(n^3)
+
+# Method 2: 
 
 # this one is very easy and logical.
 # just the another way to check whether a string is palindrome or not.
@@ -37,3 +42,32 @@ class Solution:
                 l-= 1
                 r+= 1
         return ans
+    
+# Method 2: Using DP
+# logic: 
+"""
+Observation: how we can avoid unnecessary re-computation while validating palindromes.
+ Consider the case "ababa". If we already knew that "bab" is a palindrome, it is obvious 
+ that "ababa" must be a palindrome since the two left and right end letters are the same.
+"""
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) <= 1:
+            return s
+        
+        Max_Len=1
+        Max_Str=s[0]
+        dp = [[False for _ in range(len(s))] for _ in range(len(s))] 
+        # dp[j][i] : whether string from 'j to i' is palindrome or not.
+        # j: starting index , i: last index
+        for i in range(len(s)):
+            dp[i][i] = True
+            for j in range(i):
+                if s[j] == s[i] and (i-j <= 2 or dp[j+1][i-1]):
+                    dp[j][i] = True
+                    if i-j+1 > Max_Len:
+                        Max_Len = i-j+1
+                        Max_Str = s[j:i+1]
+        return Max_Str
+
+# later try in O(n). SOlution in sheet.

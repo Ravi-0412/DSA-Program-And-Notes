3e8edcc1462960fb8e35fdf53de3671a93ebbebb
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2024-09-14 15:03:03 +0530

    Create 59. Spiral Matrix II.py

---
+++ Two Pointer/59. Spiral Matrix II.py
@@ -0,0 +1,47 @@
+# Logic: 
+"""
+Same as : "54. Spiral Matrix"
+Just keep on adding element at the cell we are visiting.
+Take a counter 'cnt' that will tell what value we have to add that cell and 
+for exit condition.
+
+"""
+
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        if n == 1:   # without this it will give TLE because count will never increment.
+            return [[1]]
+        res = [[0 for j in range(n)] for i in range(n)]
+        row, col= n, n
+        up= 0                  # rowBegin
+        down= row -1    # rowEnd
+        left= 0                # colBegin
+        right= col  -1  # colEnd
+        cnt = 1
+        while cnt <= row * col:
+            # Traverse right, proper row should be fixed
+            for c in range(left, right +1):
+                if len(res) >= row * col:
+                    break
+                res[up][c] = cnt
+                cnt += 1
+            # Traverse down, proper col should be fixed
+            for r in range(up +1, down +1):
+                if len(res) >= row * col:
+                    break
+                res[r][right] = cnt 
+                cnt += 1
+            # Traverse left, proper row should be fixed
+            for c in range(right-1, left-1, -1):
+                if len(res) >= row * col:
+                    break
+                res[down][c] = cnt 
+                cnt += 1
+            # Traverse up, proper col should be fixed
+            for r in range(down-1, up, -1):
+                if len(res) >= row * col:
+                    break
+                res[r][left] = cnt 
+                cnt += 1
+            up, down, left, right= up + 1, down -1, left + 1, right -1
+        return res

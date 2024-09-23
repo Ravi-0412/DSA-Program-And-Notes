e934af1e5aff770f6dba373b221824869ba6aac2
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2024-09-23 08:25:25 +0530

    Update 101. Symmetric Tree.py

--- Tree/Binary Tree/101. Symmetric Tree.py
+++ Tree/Binary Tree/101. Symmetric Tree.py
@@ -15,3 +15,25 @@ class Solution:
         return (root1.data==root2.data) and self.isMirror(root1.left, root2.right) and self.isMirror(root1.right, root2.left)  
                                                                 # here the diff from "identical trees"
 
+# java
+"""
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) {  // if null or contains only single node
+            return true;
+        }
+        return isMirror(root.left, root.right);  // now check for the left and right subtree  
+    }
+
+    private boolean isMirror(TreeNode root1, TreeNode root2) {
+        // if any of them is null then both should be null
+        if (root1 == null || root2 == null) {
+            return root1 == root2;
+        }
+        // Check the symmetry for values and subtrees
+        return (root1.val == root2.val) 
+                && isMirror(root1.left, root2.right) 
+                && isMirror(root1.right, root2.left);
+    }
+}
+"""

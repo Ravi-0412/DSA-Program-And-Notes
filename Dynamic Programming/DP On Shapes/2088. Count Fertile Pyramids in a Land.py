c57602f6cbeee9f2007c14de262b2ab747e409bd
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2024-09-28 22:47:21 +0530

    Update 2088. Count Fertile Pyramids in a Land.py

--- Dynamic Programming/DP On Shapes/2088. Count Fertile Pyramids in a Land.py
+++ Dynamic Programming/DP On Shapes/2088. Count Fertile Pyramids in a Land.py
@@ -31,11 +31,100 @@ class Solution:
         m , n = len(grid) , len(grid[0])
         reversed_grid = []
         for i in range(m -1, -1, -1):
-            reversed_grid.append(grid[i][:])
+            reversed_grid.append(grid[i].copy())   # deep copy
         ans =  count(grid)  # will count the ans for reverse pyramid
         ans += count(reversed_grid)  # will count the ans for simple pyramid
         return ans
 
+# Java
+"""
+class Solution {
+    public int countPyramids(int[][] grid) {
+        int[][] inverseGrid = inverse(grid);
+         return helper(grid) + helper(inverseGrid);
+    }
+    
+    public int helper(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int ans = 0;
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n - 1; j ++) {
+                if(grid[i][j] > 0) {
+                    grid[i][j] = Math.min(Math.min(grid[i-1][j], grid[i-1][j-1]), grid[i-1][j + 1]) + 1;
+                    ans += grid[i][j] - 1;
+                }
+            }
+        }
+        return ans;
+    }
+    
+    public int[][] inverse(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[][] g = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                g[i][j] = grid[m - i- 1][j];
+            }
+        }
+        return g;
+    }
+}
+"""
+
+# Note: in java if you will do like this then won't work.
+"""
+After updating the grid for one type of pyramid (regular or inverted), you are modifying the grid itself. 
+This alteration affects subsequent computations, leading to an incorrect answer.
+"""
+
+"""
+public int countPyramids(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        // Create a reversed grid for counting inverted pyramids
+        int[][] reversedGrid = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            reversedGrid[i] = grid[m - 1 - i].clone();
+        }
+
+        // Count regular pyramids
+        int ans = count(grid, m, n);
+        // Count inverted pyramids
+        ans += count(reversedGrid, m, n);
+
+        return ans;
+    }
+}
+"""
+
+# if you want to do 'using' clone the you have to use two separate 2-D arrays like below.
+# But correct one is better one as that saves one extra 2-D array.
+"""
+public int countPyramids(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        // Create a deep copy of the grid for counting inverted pyramids
+        int[][] reversedGrid = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            reversedGrid[i] = grid[m - 1 - i].clone();
+        }
+
+        // Create another deep copy of the original grid to preserve it
+        int[][] originalGrid = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            originalGrid[i] = grid[i].clone();
+        }
+
+        // Count regular pyramids using the original grid
+        int ans = count(originalGrid, m, n);
+        // Count inverted pyramids using the reversed grid
+        ans += count(reversedGrid, m, n);
+
+        return ans;
+    }
+"""
 
 # Do by prefix Sum method later and other approaches. link in sheet
 # https://leetcode.com/problems/count-fertile-pyramids-in-a-land/solutions/1606479/easy-prefix-sum-accepted-intuition-comment-without-dp/

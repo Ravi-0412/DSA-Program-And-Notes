7ec4f6e0c186662d9f13b5d9094c4b636140df96
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2024-10-03 19:23:46 +0530

    Update 224. Basic Calculator.py

--- Stack/224. Basic Calculator.py
+++ Stack/224. Basic Calculator.py
@@ -23,7 +23,8 @@ class Solution:
                     num= 0
                     while isinstance(stack[-1], int):
                         num+= stack.pop()
-                    # now perform the operation for operator in stack(just after pre '(' )
+                    # now perform the operation for poping the operator in stack(just after pre '(' )
                     update(stack.pop(), num)
-                num, lastOperator= 0, c
+                num, lastOperator= 0, c   # if 'c' is ')' then also there won't be any problem because after ')' 
+                                        # there must be any operator i.e '+' or '-' so lastOperator will be updated by '+' or  '-' only for next index.
         return sum(stack)

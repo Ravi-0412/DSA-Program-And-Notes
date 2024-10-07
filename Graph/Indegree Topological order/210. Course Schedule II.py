c7547571d92c1226dc5df8a14dbf971abcbabad1
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2024-10-07 21:52:45 +0530

    Update 210. Course Schedule II.py

--- Graph/Indegree Topological order/210. Course Schedule II.py
+++ Graph/Indegree Topological order/210. Course Schedule II.py
@@ -3,38 +3,34 @@
 
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        AdjList= defaultdict(list)
-        # first convert into adjacency list(edges) for directed graph
+        n = numCourses
+        AdjList = defaultdict(list)
+        indegree = [0]*n
+        # first convert into adjacency list(edges) for directed graph and calculate indegree
         for second,first in prerequisites:
             AdjList[first].append(second)
+            indegree[second] += 1
         
-        count,Q,ans,n= 0, [], [], numCourses
-        indegree= [0]*n
-        
-        # finding the indegree of each vertices
-        for i in range(n):
-            for k in AdjList[i]:  # if k is adj to 'i' means there is one indegree edge to 'k'
-                indegree[k]+= 1
-        
-        
+        count, ans = 0, []
+        Q = collections.deque()
         # now applying the BFS to get the topological order
         # find the node with indegree '0' as this node will come 1st in the topological order
         # i.e it will be the source node and after that apply the BFS
         for i in range(n):
-            if indegree[i]==0: 
+            if indegree[i] == 0: 
                 Q.append(i)
     
         while Q:
-            count+= 1  
-            u= Q.pop(0)
+            count += 1  
+            u = Q.popleft()
             ans.append(u)
             # after poping decrease the indegree of all node adjacent to 'u'
             for j in AdjList[u]:
-                indegree[j]-= 1
-                if indegree[j]== 0:  # after decreasing if any node has indegree == 0 then put in the Q
+                indegree[j] -= 1
+                if indegree[j] == 0:  # after decreasing if any node has indegree == 0 then put in the Q
                     Q.append(j)
 
-        if count!= n:  # means cycle so no order is possible 
+        if count != n:  # means cycle so no order is possible 
             return []
         return ans
 

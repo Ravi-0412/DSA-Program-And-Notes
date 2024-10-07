bc9b4cc96c0ea345c1f15f207e45d818b39df0b0
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2024-10-07 19:56:38 +0530

    Update 2222. Number of Ways to Select Buildings.py

--- Dynamic Programming/String Matching/2222. Number of Ways to Select Buildings.py
+++ Dynamic Programming/String Matching/2222. Number of Ways to Select Buildings.py
@@ -1,1 +1,4 @@
+ # Ans: There will be only two possibility i.e '101' and '010' so Q reduces to
+# find the number of distinct subsequences of '101' and '010' in string and add them.
 
+# Which is same as Question: "115. Distinct Subsequences".

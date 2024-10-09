2df10a3ff30d34ccbfe05ccc758bf5799bc105f2
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2024-10-10 02:30:01 +0530

    Update 988. Smallest String Starting From Leaf.py

--- Tree/Binary Tree/988. Smallest String Starting From Leaf.py
+++ Tree/Binary Tree/988. Smallest String Starting From Leaf.py
@@ -1,7 +1,8 @@
 # Very good problem to keep in mind. Will help a lot in future.
 
 # My mistake:
-# I was doing using bottom up i.e: answer(root) = min(answer(left) + root.val, answer(right) + root.val)
+"""
+I was doing using bottom up i.e: answer(root) = min(answer(left) + root.val, answer(right) + root.val)
 # But it won't work because:
 # At current node you may ignore either left or right part after comparison but
 # the part you ignored can be better ans than the part you you returned at later node.
@@ -17,7 +18,8 @@
 # try exmaple:
 # 1) [4,0,1,1]. ans should be : "bae" but it will give "ae".
 # 2)  [25, 1, null, 0, 0, 1, null, null, null, 0].  expected answer is "ababz" but it will give "abz".
-
+# 3) root = [0,null,1], Output : "a" , Expected : "ba"  => will return ""(empty which is not allowed) from left but it should return "b".
+""" 
 # Fore more visualisation go through: https://leetcode.com/problems/smallest-string-starting-from-leaf/solutions/244205/divide-and-conquer-technique-doesn-t-work-for-this-problem/?envType=daily-question&envId=2024-04-17
 
 
@@ -34,8 +36,8 @@ class Solution:
             smallest = l + chr(root.val + 97)
         return smallest
 
-
-# So only way it to track the path till leaf and then compare
+# How to solve this problem?
+# So only way it to track the path till leaf and then compare. Top Down
 
 class Solution:
     def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
@@ -54,3 +56,36 @@ class Solution:
         
         dfs(root, "")
         return self.ans
+
+# Java
+"""
+class Solution {
+    private String ans = "";  // Use a class-level variable to track the smallest string
+
+    public String smallestFromLeaf(TreeNode root) {
+        dfs(root, "");  // Start DFS with an empty string
+        return ans;
+    }
+
+    private void dfs(TreeNode root, String s) {
+        if (root == null) {
+            return;
+        }
+        
+        // Convert the current node's value to the corresponding character
+        s = (char)(root.val + 'a') + s;  // Prepend current char to the string
+
+        // If we are at a leaf node
+        if (root.left == null && root.right == null) {
+            if (ans.equals("") || ans.compareTo(s) > 0) {
+                ans = s;  // Update ans if current string is smaller
+            }
+            return;
+        }
+
+        // Recur for left and right subtrees
+        dfs(root.left, s);
+        dfs(root.right, s);
+    }
+}
+"""

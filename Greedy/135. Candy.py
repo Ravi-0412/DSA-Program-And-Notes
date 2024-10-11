f67401db96a75301c473efd6aea0de74a29a4d12
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2024-10-11 12:38:41 +0530

    Update 135. Candy.py

--- Greedy/135. Candy.py
+++ Greedy/135. Candy.py
@@ -31,5 +31,57 @@ class Solution:
         return sum(ans)
 
 
-# Try in O(1) space
-# https://leetcode.com/problems/candy/solutions/42770/one-pass-constant-space-java-solution/
+# Method 2: Optimising to O(1) space
+# Logic:
+"""
+The fundamental idea is that we aren't bothering with calculating each child's 
+candy value itself, but rather it's impact on the total needed.
+For upwards and equal slopes these values align. 
+But for the downward slope we keep track of how much more candies we have to add to the previous kids 
+to satisfy the requirement of them having more candy than lower rated neighbours to the right.
+
+This method is basically answering this question: If I insert one more kid to the end of this array, 
+how does the total amount of candy needed change.
+"""
+
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        # If there are no children (empty ratings list), return 0
+        if len(ratings) == 0:
+            return 0
+        
+        # Initialize variables:
+        # ret: total number of candies needed (starting with 1 for the first child)
+        # up: length of the current increasing sequence of ratings
+        # down: length of the current decreasing sequence of ratings
+        # peak: length of the last peak in an increasing sequence
+        ret = 1
+        up = 0
+        down = 0
+        peak = 0
+        
+        # Loop through the ratings list starting from the second child (index 1)
+        for i in range(1, len(ratings)):
+            if ratings[i - 1] < ratings[i]:
+                # If current rating is greater than the previous one, we are in an increasing sequence
+                up += 1        # Increase the "up" counter
+                peak = up      # Update the peak value
+                down = 0       # Reset the "down" counter since we are in the increasing streak 
+                ret += 1 + up  # Add candies for the current child and all previous in the increasing sequence
+            elif ratings[i - 1] == ratings[i]:
+                # If current rating is equal to the previous one, just give 1 candy (no slope)
+                peak = up = down = 0  # Reset all counters
+                ret += 1              # Add 1 candy for the current child
+            else:
+                # If current rating is less than the previous one, we are in a decreasing sequence
+                up = 0            # Reset the "up" counter since the decreasing streak starts
+                down += 1         # Increase the "down" counter
+                # Add candies for the current child and adjust based on whether we are still above the peak
+                ret += down
+                # if peak is not large enough, then we need to make peak larger
+                if peak < down:
+                    ret += 1
+
+        # Return the total number of candies required
+        return ret
+        

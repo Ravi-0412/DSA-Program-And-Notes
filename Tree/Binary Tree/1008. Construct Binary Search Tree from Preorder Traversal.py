f8d3a6bb4217fd9001f40afc3632a4a7df59f9ed
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2024-10-12 15:44:03 +0530

    Update 1008. Construct Binary Search Tree from Preorder Traversal.py

--- Tree/Binary Tree/1008. Construct Binary Search Tree from Preorder Traversal.py
+++ Tree/Binary Tree/1008. Construct Binary Search Tree from Preorder Traversal.py
@@ -1,8 +1,34 @@
-# just sort the array to get the inorder and then apply the same logic as binary Tree.
+# Method 1:
+#  just find out the inorder traversal by sorting the array and Apply "convert into binary tree given preorder and inorder"
+# of binary tree.
 # Time: O(n* logn)
 
-# Method 2:
-# Time = O(n), space = O(1)
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
+        inorder= sorted(preorder)
+        return self.buildTree(preorder, inorder)
+    
+    def buildTree(self, preorder, inorder):
+        if not inorder: # or preorder
+            return None
+        root= TreeNode(preorder[0])   # this will be the root
+
+        # find the index of 1st ele of prerorder in inorder to check all nodes will go the left of parent and right of parent.
+        ind= inorder.index(preorder[0])
+        # all the ele from index '1' till 'ind' will come to the left in preorder(1st ele already included.) and all the ele before the indx in inorder will come to the left subtree(ind ele already included)
+        root.left=  self.buildTree(preorder[1:ind+1], inorder[:ind])
+        # all the ele after the indx will come to right of both preorder and inorder.
+        root.right= self.buildTree(preorder[ind+1 :], inorder[ind+1 :])                
+
+        return root
+
+# method 2: simply sort and then apply the "convert the given sorted array into Balanced BST".
+# time: O(n*logn) for both methods.
+
+# method 3: Take each ele in preorder and apply the normal method to form BST i.e insert node in BST one by one.
+# time: O(n*logn).
+
+# Method 4:
 
 """
 Idea is simple: 
@@ -11,6 +37,24 @@ Idea is simple:
 2.a) If value is less than last item in stack, it is the left child of last item.
 2.b) If value is greater than last item in stack, pop it.
       The last popped item will be the parent and the item will be the right child of the parent.
+"""
+
+"""
+How to think of this logic?
+# Q. How to come up with this logic?
+# Ans: Just given the preorder, draw the BST on paper and analyse how you are putting the nodes and 
+# which Data Structure we can use to get BST directly from preorder.
+
+# Q. why we are directly adding when num is samller and poping before adding when num is greater?
+# Ans: if smaller means that must be the left child of the just previous ele in stack , 
+# Because we are doing acc to the preorder and in preorder we will move to left after root and 
+# it is BST so smaller ele will be on left.(root, left right,)
+# vvi: until we find any ele greater than top of stack, all those num will be go as left child only(like skew tree).
+# And we will find any ele greater then we will search for the node to which 'num' will be the right child.  
+# (now direction of tree will change).
+# i.e we will find the last smaller ele from the current num. 'num' will be the right child of that ele.
+
+# That's why we are using stack.
 """
 
 # Time = space = O(n)
@@ -61,7 +105,7 @@ class Solution {
 """
 
 
-# Method 2: Optimising space to O(n)
+# Method 5: Optimising space to O(1)
 # Time: O(n), space = O(1) 
 
 class Solution:

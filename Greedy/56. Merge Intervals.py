2a5c90d5dd5f58628f08f38bea77fa0278913f00
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2024-10-14 20:34:44 +0530

    Update 56. Merge Intervals.py

--- Greedy/56. Merge Intervals.py
+++ Greedy/56. Merge Intervals.py
@@ -12,10 +12,12 @@ class Solution:
         # intervals.sort(key= lambda i: i[0])   # 'i': intervals, i[0] index value on which we want to sort
         output= [intervals[0]]  # to handle the edge case and make comparison easy
         for start, end in intervals[1:]:
-            # check if ending of last added interval is >= than starting of the currnet one
-            if output[-1][1]>= start: # then merge , make end of last added one max(end of last addded, end)
+            # check if ending of last added interval is >= than starting of the currnet one 
+            if output[-1][1]>= start: 
+		# i.e if overlapping then merge , make end of last added one max(end of last addded, end)
                 output[-1][1]= max(output[-1][1], end)    # [[1,4],[2,3]]= [[1,4]]
-            else: 
+            else:
+		# If not overlapping then add directly 
                 output.append([start, end])  # just the cur interval only.
         return output
 

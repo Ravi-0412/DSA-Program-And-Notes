d6d48bda496d62eac9df4f042b7f9a36072e1b0c
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2024-10-24 03:45:06 +0530

    Update 3101. Count Alternating Subarrays.py

--- Sliding  Window/Variable Size Sliding Window/3101. Count Alternating Subarrays.py
+++ Sliding  Window/Variable Size Sliding Window/3101. Count Alternating Subarrays.py
@@ -1,14 +1,27 @@
+# Logic:
+"""
+At each index, find the length of alternating subarray.
+Say length = n after adding cur ele 'i; then we will get 'n' new subarray.
+
+e.g: 
+
+[1, 0, 1, 0, 1]
+   [0, 1, 0, 1]
+      [1, 0, 1]
+	     [0, 1]
+	        [1]
+"""
+# Time = O(n), space = O(1)
 class Solution:
     def countAlternatingSubarrays(self, nums: List[int]) -> int:
         n = len(nums)
-        i, j = 0, 1
-        ans = 1
-        while j < n:
-            if nums[j] != nums[j -1]:
-                ans += j -i + 1   
+        size, ans, i = 1 , 1, 1
+        while i < n:
+            if nums[i] != nums[i - 1]:
+                size += 1
+                ans += size
             else:
-                ans += 1
-                i = j    # For next window we need to check from 'j'
-            j += 1
+                size = 1
+                ans += size
+            i += 1
         return ans
-        

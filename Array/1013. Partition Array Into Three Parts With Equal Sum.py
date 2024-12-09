61715175c14588b5d90ed8a2f1c809dc380c465a
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2024-12-09 08:47:21 +0530

    Update 1013. Partition Array Into Three Parts With Equal Sum.py

--- Array/1013. Partition Array Into Three Parts With Equal Sum.py
+++ Array/1013. Partition Array Into Three Parts With Equal Sum.py
@@ -17,3 +17,27 @@ class Solution:
                 partsSum = 0
                 partsCount += 1
         return partsCount >= 3     # '>=' to handle the case like [0,0,0,0]
+
+# Method 2: 
+"""
+Logic: 
+Staring from the two ends of the input array, accumulate left and right parts till getting the average values;
+If having found average values on both parts before the two pointers meet, return true; otherwise, return false.
+"""
+
+class Solution:
+    def canThreePartsEqualSum(self, A: List[int]) -> bool:
+        l, r, s = 1, len(A) - 2, sum(A)
+        if s % 3 != 0:
+            return False
+        leftSum, rightSum, average = A[0], A[-1], s // 3
+        while l < r:
+            if l < r and leftSum != average:
+                leftSum += A[l]
+                l += 1
+            if l < r and rightSum != average:
+                rightSum += A[r]
+                r -= 1
+            if leftSum == average == rightSum:
+                return True    
+        return False

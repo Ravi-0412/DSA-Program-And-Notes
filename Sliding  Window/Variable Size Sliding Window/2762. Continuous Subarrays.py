937fc15101576009063df844a62b5bd5797621ed
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2024-12-16 19:44:13 +0530

    Update 2762. Continuous Subarrays.py

--- Sliding  Window/Variable Size Sliding Window/2762. Continuous Subarrays.py
+++ Sliding  Window/Variable Size Sliding Window/2762. Continuous Subarrays.py
@@ -29,7 +29,7 @@ class Solution:
                 if hashmap[nums[i]] == 0:  # removing the ele from left side
                     del hashmap[nums[i]]
                 i += 1
-            ans += j - i + 1
+            ans += j - i + 1   # length of valid subarray
             j += 1
         return ans
 
@@ -39,7 +39,7 @@ class Solution:
 
 # Reason: We need to keep track of min and max values so for this we can use two heaps 'minHeap' and 'maxHeap'.
 # For knowing the index i.e where to move from left in case of invalid one, 
-# we will also store the 'index + 1' with ele value. This will tell where to move for valid one.
+# we will also store the 'index + 1' with ele value. This will tell where to move for valid one, when we will get invalid one.             
 
 # While adding any ele first we will check the valid condition wrt both minimum & max ele till now.
 # and keep on poping till you get the valid window wrt both(min and max).
@@ -76,3 +76,47 @@ class Solution:
             j += 1
         return ans
 
+# Method 3: best one
+# Time: O(n)
+
+# Logic:
+"""
+Link: https://leetcode.com/problems/continuous-subarrays/solutions/6144580/easyyyyy-pessyyy-approch/
+
+Explanation:
+1) Sliding Window:
+The right pointer (r) will iterate over the array, extending the window.
+For each element, we update the window by adding it to our deques (one for the minimums and one for the maximums).
+2) Maintain the Deques:
+The minD deque stores indices of elements in increasing order of their values, ensuring that nums[minD.peekFirst()] is always the smallest value in the window.
+The maxD deque stores indices of elements in decreasing order of their values, ensuring that nums[maxD.peekFirst()] is always the largest value in the window.
+3) Shrinking the Window:
+If the difference between the maximum and minimum values (tracked by the front of the deques) exceeds 2, we increment the left pointer (l) and remove any indices from the deques that are out of the new window.
+4) Count Valid Subarrays:
+For each valid window, the number of subarrays that end at r and start anywhere between l and r is r - l + 1.
+"""
+
+class Solution:
+    def continuousSubarrays(self, nums: List[int]) -> int:
+        l, res = 0, 0
+        minD, maxD = deque(), deque()
+        
+        for r in range(len(nums)):
+            # to maintain 'indices of elements in increasing order of their values' in 'minD'
+            while minD and nums[minD[-1]] >= nums[r]: minD.pop()
+            # to maintain 'indices of elements in increasing order of their values' in 'maxD'
+            while maxD and nums[maxD[-1]] <= nums[r]: maxD.pop()
+            minD.append(r)
+            maxD.append(r)
+
+            # till invalid subarray
+            while nums[maxD[0]] - nums[minD[0]] > 2:
+                l += 1  # increment from 'left'
+                # if out of valid subarray so remove from both 'minD' and 'maxD'
+                if minD[0] < l: minD.popleft()  
+                if maxD[0] < l: maxD.popleft()
+            
+            res += r - l + 1  # length of valid subarray
+        
+        return res
+        

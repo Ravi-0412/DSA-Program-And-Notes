b79294d066e2d43282a8192ef32323c6aeda6906
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-01-18 08:57:21 +0530

    Update 2683. Neighboring Bitwise XOR.py

--- Bitwise_Manipulation/2683. Neighboring Bitwise XOR.py
+++ Bitwise_Manipulation/2683. Neighboring Bitwise XOR.py
@@ -1,16 +1,23 @@
 # logic: 
 
-# orginal: A[0], A[1], .... A[n-1]
-# derived: A[0]^A[1], A[1]^A[2] .... A[n-1]^A[0]
+"""
+orginal: A[0], A[1], .... A[n-1]
+derived: A[0]^A[1], A[1]^A[2] .... A[n-1]^A[0]
 
-# xor(derived) = (A[0]^A[1])^(A[1]^A[2])^ .... ^(A[n-1]^A[0]) = 0
+xor(derived) = (A[0]^A[1])^(A[1]^A[2])^ .... ^(A[n-1]^A[0]) = 0
 
-# The necessary and suffisant condition for derived to have an original is
-# xor(derived) == 0
-# Note: this can apply to any q of this type.
+The necessary and sufficient condition for derived to have an original is
+xor(derived) == 0
 
-# When original and derived is binary sequence,
-# this equals to sum(derived) % 2 == 0.
+And this will be only = 0 if all numbers will be present in even freqeuency.
+But since here array is binary so we can also say that frequency of one should be even.
+or 'sum(derived) % 2 == 0'.
+
+So When original and derived is binary sequence,
+this equals to sum(derived) % 2 == 0.
+
+Note: this can apply to any q of this type.
+"""
 
 # time: O(n)
 class Solution:

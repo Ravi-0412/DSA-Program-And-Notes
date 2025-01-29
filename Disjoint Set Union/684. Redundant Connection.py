1f8c4d6a74a138376d9d6b85aa7723ff874d180c
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-01-30 00:38:30 +0530

    Update 684. Redundant Connection.py

--- Disjoint Set Union/684. Redundant Connection.py
+++ Disjoint Set Union/684. Redundant Connection.py
@@ -1,8 +1,14 @@
 
 # using Disjoint Set union.
 # This Q was based on this logic only.
-# logic: all edges connected together should not lead to cycle, so
-# whenver we find any two node for which union is not possible then return those node & that will be our ans.
+# logic: 
+"""
+all edges connected together should not lead to cycle, so
+whenver we find any two node for which union is not possible then return those node & that will be our ans.
+
+Also we need to return the answer that occurs last in the input, If there are multiple answers.
+And this will get handled automatically
+"""
 
 # time for union: O(4 alpha) nearly = O(1), space: O(n)
 # time for find Parent= O(4* alpha)
@@ -36,6 +42,68 @@ class Solution:
         for n1,n2 in edges:
             if dsu.unionBySize(n1,n2)== False:   # if you find any edge for which we can't do umio simply return that.
                 return [n1,n2]
+
+# Java
+import java.util.*;
+
+class DSU {
+    int[] parent;
+    int[] size;
+    
+    public DSU(int n) {
+        parent = new int[n];
+        size = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+    
+    public int findUPar(int n) {
+        if (n == parent[n]) {
+            return n;
+        }
+        parent[n] = findUPar(parent[n]);  // Path compression
+        return parent[n];
+    }
+    
+    public boolean unionBySize(int n1, int n2) {
+        int p1 = findUPar(n1);
+        int p2 = findUPar(n2);
+        
+        if (p1 == p2) {
+            return false;  // Already in the same set
+        }
+        
+        if (size[p1] < size[p2]) {
+            parent[p1] = p2;
+            size[p2] += size[p1];
+        } else {
+            parent[p2] = p1;
+            size[p1] += size[p2];
+        }
+        
+        return true;
+    }
+}
+
+public class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        DSU dsu = new DSU(n + 1);  // n + 1 for 1-based indexing
+        
+        for (int[] edge : edges) {
+            int n1 = edge[0];
+            int n2 = edge[1];
+            
+            if (!dsu.unionBySize(n1, n2)) {
+                return edge;  // Return the redundant edge
+            }
+        }
+        
+        return new int[] {};  // In case there's no redundant connection
+    }
+}
 
 
 

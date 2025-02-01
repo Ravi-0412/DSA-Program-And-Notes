cbb9105bb9477f5776b701ce51c95b23607d44cd
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-02-01 19:34:21 +0530

    Update 547. Number of Provinces.py

--- Disjoint Set Union/547. Number of Provinces.py
+++ Disjoint Set Union/547. Number of Provinces.py
@@ -1,7 +1,30 @@
 # just we have to find the 'no of connected components' cell value= 1.
 # we can use either Bfs or dfs for this.
 
-# By DSU.
+# Method 1: DFS
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n= len(isConnected)
+        count= 0
+        visited= set()
+
+        def dfs(i):
+            # any other city will be only connected if there is value= '1' between that city.
+            # also we have to check whether other city is already visited(already in one component).
+            for j in range(n):
+                if isConnected[i][j] ==1 and j not in visited:
+                    visited.add(j)
+                    dfs(j)  # connecting all cities together whcih are indirectly connected
+
+        # from each noode try to get all the nodes connected to each other.
+        for city in range(n):
+            if city not in visited:
+                count+= 1
+                dfs(city)
+        
+        return count
+
+# Method 2: By DSU.
 # logic: just count the no of unique ultimate parent. This will give the no of connected components.
 # for this just fill the parent array then then run a for loop to check :
 # if n= parent[n]: coun+= 1. and at last return count
@@ -15,6 +38,22 @@
 
 # how to do union : Try to bring all the connected nodes(value==1) into one component.
 # if we can't do union of any two node their parent node will differ and will go into other component.
+
+"""
+# Note: This method of counting distinct parent is always valid to count the connected components but in other Q you have to modify a bit.
+
+# Note for union: Q) Here we are not doing union with any previous ele but we only do union with already added ele or 
+# we can do union with one connected component with another connected component. But why still giving correct ans?
+
+# Reason: Because whenever there is '1' between any two node means they are connected to each other. 
+# And a single node is connected componnet in itself. So we are trying to connect two connected component only.
+# So the node which is directly or indirectly connected('1' between them) will form a single component.
+# e.g: [0][1]= 1 then after union (0,1) will come together. now let [1][3]= 1 then '3' will get attached to parent of '1' and finally 
+# all three (0,1,3) will get connected into one single component.  In this way we will get all connected nodes.
+
+# Q) Why we are making parent array of 'n' only desite we are working on 'n*n' matrix?
+# Reason: Because there can be max 'n' connected components(only 'n' vertices).
+"""
 
 # time: O(n^2) to traverse into matrix.
 
@@ -66,42 +105,4 @@ class Solution:
         return count
 
 
-# Note: This method of counting distinct parent is always valid to count the connected components but in other Q you have to modify a bit.
-
-# Note for union: Q) Here we are not doing union with any previous ele but we only do union with already added ele or 
-# we can do union with one connected component with another connected component. But why still giving correct ans?
-
-# Reason: Because whenever there is '1' between any two node means they are connected to each other. 
-# And a single node is connected componnet in itself. So we are trying to connect two connected component only.
-# So the node which is directly or indirectly connected('1' between them) will form a single component.
-# e.g: [0][1]= 1 then after union (0,1) will come together. now let [1][3]= 1 then '3' will get attached to parent of '1' and finally 
-# all three (0,1,3) will get connected into one single component.  In this way we will get all connected nodes.
-
-# Q) Why we are making parent array of 'n' only desite we are working on 'n*n' matrix?
-# Reason: Because there can be max 'n' connected components(only 'n' vertices).
-
-
-
-# Method 2: Dfs solution of above.
-class Solution:
-    def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        n= len(isConnected)
-        count= 0
-        visited= set()
-
-        def dfs(i):
-            # any other city will be only connected if there is value= '1' between that city.
-            # also we have to check whether other city is already visited(already in one component).
-            for j in range(n):
-                if isConnected[i][j] ==1 and j not in visited:
-                    visited.add(j)
-                    dfs(j)  # connecting all cities together whcih are indirectly connected
-
-        # from each noode try to get all the nodes connected to each other.
-        for city in range(n):
-            if city not in visited:
-                count+= 1
-                dfs(city)
-        
-        return count
         

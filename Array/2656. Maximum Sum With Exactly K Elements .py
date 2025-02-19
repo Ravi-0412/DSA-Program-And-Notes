8c33459e1953e886ec4fa02328c3b4c4553336e3
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-02-19 20:14:36 +0530

    Update 2656. Maximum Sum With Exactly K Elements .py

--- Array/2656. Maximum Sum With Exactly K Elements .py
+++ Array/2656. Maximum Sum With Exactly K Elements .py
@@ -1,6 +1,8 @@
 # to get the maximum sum in 'k' operation we will pick each time max ele only.
 # for this 1st find the max ele and then each operation we have to incr this.
 
+# Time = Max(n, k)
+
 class Solution:
     def maximizeSum(self, nums: List[int], k: int) -> int:
         max_ele= max(nums)
@@ -9,3 +11,9 @@ class Solution:
             sum+= max_ele
             max_ele+= 1  # incr each time max_ele by 1
         return sum
+
+# Other way
+# Logic: Find maximum element + formula 
+class Solution:
+    def maximizeSum(self, n: List[int], k: int) -> int:
+        return  k * max(n) + k * (k - 1) // 2;

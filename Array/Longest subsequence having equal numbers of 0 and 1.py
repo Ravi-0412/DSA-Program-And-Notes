78309b9438b8ebe8062ec54ac582cbd19a9d3a3a
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-03-07 22:27:46 +0530

    Update Longest subsequence having equal numbers of 0 and 1.py

--- Array/Longest subsequence having equal numbers of 0 and 1.py
+++ Array/Longest subsequence having equal numbers of 0 and 1.py
@@ -1,7 +1,13 @@
 # logic: 
 """
  count zeros & ones in a binary array and last return minimum between count of zeros & ones by multiplying it with 2. 
+ Reason: Since binary array and we are asked to find subsequence(not subarray), so we can take take min(no_one, no_zero) 
+ and make a valid subsequence from those numbers. so ans = min(no_one, no_zero) * 2.
+
+ Note: Same Question for longest subarray. (525. Contiguous Array)
 """
+
+# Time = O(n), space = O(1)
 
 def largestSubsequence(arr,n):
     # store count of zeros and one

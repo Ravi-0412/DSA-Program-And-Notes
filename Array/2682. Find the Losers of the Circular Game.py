aeb451ad2484346fa9ac3417c91733a4598e6c33
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-03-14 08:54:17 +0530

    Update 2682. Find the Losers of the Circular Game.py

--- Array/2682. Find the Losers of the Circular Game.py
+++ Array/2682. Find the Losers of the Circular Game.py
@@ -28,5 +28,47 @@ class Solution:
                 ans.append(i)
         return ans
 
+# java
+"""
+import java.util.ArrayList;
+import java.util.List;
 
-# Note: We can use 'set()' also.
+class Solution {
+    public int[] circularGameLosers(int n, int k) {
+        int i = 1; // turn
+        int[] count = new int[n + 1]; // to check where we reach 2 times (1-based indexing)
+        count[1] = 1; // starting from person '1'
+        int curNum = 1;
+        
+        while (true) {
+            // next person
+            curNum = (curNum + i * k) % n;
+            // using 1-based indexing, but modulus with 'n' means '0' should mean 'n'
+            if (curNum == 0) {
+                curNum = n;
+            }
+            count[curNum]++;
+            if (count[curNum] == 2) {
+                break;
+            }
+            i++;
+        }
+        
+        List<Integer> temp = new ArrayList<>();
+        for (int j = 1; j <= n; j++) {
+            if (count[j] == 0) {
+                temp.add(j);
+            }
+        }
+        
+        // Convert List<Integer> to int[]
+        int[] ans = new int[temp.size()];
+        for (int j = 0; j < temp.size(); j++) {
+            ans[j] = temp.get(j);
+        }
+        
+        return ans;
+    }
+}
+
+"""

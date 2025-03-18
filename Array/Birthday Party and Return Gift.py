cce14fd2855661096f40e31d05aa4678dde6dcc4
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-03-18 18:56:44 +0530

    Update Birthday Party and Return Gift.py

--- Array/Birthday Party and Return Gift.py
+++ Array/Birthday Party and Return Gift.py
@@ -1,5 +1,6 @@
 def findKid(N, T, D):
     # first find the anser for 0- based indexing in this type of cyclic question.
-    ans = (D - 1 + T- 1) % N
+    # (D + T- 1) : last one will be this person only because it will start from 'Dth person' so subtracting '-1'.
+    ans = ((D + T- 1) -1) % N   # Again subtracting '-1' for bring into '0 - based' indexing.
     # for converting into 1-based indexing add '1'.
     return ans + 1

9c7a6a3ff972811f53c35f26a4783498069e748d
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-03-21 01:28:37 +0530

    Update 2654. Minimum Number of Operations to Make All Array Elements Equal to 1.py

--- Array/2654. Minimum Number of Operations to Make All Array Elements Equal to 1.py
+++ Array/2654. Minimum Number of Operations to Make All Array Elements Equal to 1.py
@@ -1,16 +1,18 @@
-# logic: we can only make all ele equal to "1" by gcd if there is '1' already present in the array or we can make any ele =1 somehow.
+"""
+logic: we can only make all ele equal to "1" by gcd if there is '1' already present in the array or we can make any ele =1 somehow.
 
-# cases: 1) if there is already "1' is present in the array then
-# ans= n- no_of_1. we can make other remaining ele(non-1)  =1 by taking gcd with its adjacent ele(here next adjacent ele).
+cases: 1) if there is already "1' is present in the array then
+ans= n- no_of_1. we can make other remaining ele(non-1)  =1 by taking gcd with its adjacent ele(here next adjacent ele).
 
-# 2) If there is no "1", then ans= x + (n-1), where x= min cost of making any ele of any subarray =1 by doing gcd operation.
-# for this we need to find the smallest subarray we for which we can make any ele of that subarray= 1.
-# say for subarray [i, j] we can make any ele of this subarray =1 then cost= j- i  (i, j are index).
-# we will have to do gcd operation "j-i" times.
+2) If there is no "1", then ans= x + (n-1), where x= min cost of making any ele of any subarray =1 by doing gcd operation.
+for this we need to find the smallest subarray we for which we can make any ele of that subarray= 1.
+say for subarray [i, j] we can make any ele of this subarray =1 then cost= j- i  (i, j are index).
+we will have to do gcd operation "j-i" times.
 
-# for solving, we will check every possible subarray from each index and will try to make gcd=1 including all its next ele one by one.
-# time= O(n^2 * logn)  
-# for time complexity gcd: https://codeforces.com/blog/entry/48417  
+for solving, we will check every possible subarray from each index and will try to make gcd=1 including all its next ele one by one.
+time= O(n^2 * logn)  
+for time complexity gcd: https://codeforces.com/blog/entry/48417  
+"""
 
 import math
 class Solution:

edca2969ca475ca56246a3344eaff572fdd98f37
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-03-27 20:14:07 +0530

    Update 2716. Minimize String Length.py

--- String/2716. Minimize String Length.py
+++ String/2716. Minimize String Length.py
@@ -4,3 +4,26 @@
 class Solution:
     def minimizedStringLength(self, s: str) -> int:
         return len(set(s))
+
+# java
+"""
+class Solution {
+    public int minimizedStringLength(String s) {
+        return new HashSet<Character>() {{
+            for (char c : s.toCharArray()) add(c);
+        }}.size();
+    }
+}
+"""
+
+"""
+class Solution {
+    public int minimizedStringLength(String s) {
+        HashSet<Character> uniqueChars = new HashSet<>();
+        for (char c : s.toCharArray()) {
+            uniqueChars.add(c);
+        }
+        return uniqueChars.size();
+    }
+
+"""

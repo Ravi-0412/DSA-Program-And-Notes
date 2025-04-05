5d770ce51221375bd0ca687af81fb6ae22a201ff
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-04-05 09:21:38 +0530

    Update and rename BFS.PY to BFS.py

--- Graph/Basic Graph/BFS.py
+++ Graph/Basic Graph/BFS.py
@@ -85,3 +85,106 @@ g.addEdge(5,7)
 g.addEdge(6,7)
 g.print_matrix()
 g.BFS(2)
+
+
+# Java
+"""
+import java.util.*;
+
+class Graph {
+    private int size;
+    private int[] distance;
+    private String[] colors;
+    private int[] pred;
+    private int[][] adjMatrix;
+
+    public Graph(int n) {
+        size = n;
+        distance = new int[n];
+        colors = new String[n];
+        Arrays.fill(colors, "white");
+        pred = new int[n];
+        Arrays.fill(pred, -1);
+        adjMatrix = new int[n][n];
+    }
+
+    public void addEdge(int v1, int v2) {
+        if (v1 == v2) {
+            System.out.println("Both vertices are the same.");
+        } else {
+            adjMatrix[v1][v2] = 1;
+            adjMatrix[v2][v1] = 1;
+        }
+    }
+
+    public void printMatrix() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(adjMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void removeEdge(int v1, int v2) {
+        if (adjMatrix[v1][v2] == 0) {
+            System.out.println("No edge exists between these vertices.");
+        } else {
+            adjMatrix[v1][v2] = 0;
+            adjMatrix[v2][v1] = 0;
+        }
+    }
+
+    public void BFS(int s) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int i = 0; i < size; i++) {
+            graph.put(i, new ArrayList<>());
+            for (int j = 0; j < size; j++) {
+                if (adjMatrix[i][j] == 1) {
+                    graph.get(i).add(j);
+                }
+            }
+        }
+
+        colors[s] = "gray";
+        distance[s] = 0;
+        pred[s] = -1;
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(s);
+
+        System.out.println("BFS traversal of the following graph is: ");
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            System.out.print(curr + " ");
+
+            for (int neighbor : graph.get(curr)) {
+                if (colors[neighbor].equals("white")) {
+                    colors[neighbor] = "gray";
+                    distance[neighbor] = distance[curr] + 1;
+                    pred[neighbor] = curr;
+                    q.add(neighbor);
+                }
+            }
+            colors[curr] = "black";
+        }
+
+        System.out.println("\nDistance of all vertices from source: " + Arrays.toString(distance));
+    }
+
+    public static void main(String[] args) {
+        Graph g = new Graph(6);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 3);
+        g.addEdge(3, 4);
+        g.addEdge(4, 5);
+
+        System.out.println("Adjacency Matrix:");
+        g.printMatrix();
+
+        g.BFS(0);
+    }
+}
+"""

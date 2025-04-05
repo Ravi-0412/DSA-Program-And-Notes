3e00fb89dd7f0aa77b3299859921fc99fd8cee6d
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-04-05 10:01:29 +0530

    Update and rename Topologolical_sort.py to Topological_sort.py

--- Graph/Indegree Topological order/Topological_sort.py
+++ Graph/Indegree Topological order/Topological_sort.py
@@ -1,84 +1,103 @@
 # method 1: using DFS
-#logic:  just run the DFS and when there is no adjacent node put the node into the stack
-# At last print the stack in opposite direction
+"""
+logic:  just run the DFS and when there is no adjacent node put the node into the stack
+At last print the stack in opposite direction.
 
-# why came with DFS? 
-# Ans: as DFS go deeper and deeper and we need to print the node with lesser outorder vertices first
-# means stop at node with no adjacent node(no outgoing vertices as DAG will must contain,
-#  at least one vertex with outgoing edge = 0 and incoming edge =0).
+why came with DFS? 
+Ans: as DFS go deeper and deeper and we need to print the node with lesser outorder vertices first
+means stop at node with no adjacent node(no outgoing vertices as DAG will must contain,
+ at least one vertex with outgoing edge = 0 and incoming edge =0).
 
-# i.e we have to print the vertex with no outgoing edge at last and that can be done 
-# while traversing back in case of DFS.
+i.e we have to print the vertex with no outgoing edge at last and that can be done 
+while traversing back in case of DFS.
 
-# Note(VVI) for dfs method: here you are putting the node in the ans(stack), while traversing back that's why it's giving correct ans always
-# but if we put the node at start itself in the ans like when you are calling the dfs for that node then it will not give the corect ans always...
-# keep this in mind.. if you do like this graph like test case 2 will not work
+Note(VVI) for dfs method: here you are putting the node in the ans(stack), while traversing back that's why it's giving correct ans always
+but if we put the node at start itself in the ans like when you are calling the dfs for that node then it will not give the corect ans always...
+Adding the node at start won't guarantee the correct order.
+keep this in mind.. if you do like this graph like test case 2 will not work
 
-# if you do by finding the indegree like BFS then that will always give the correct ans as that is the basic of tolopogical sort.
+Explanation for adding at Last and why not at start.
+Why Post-order Works for Topological Sorting ?
 
-# Note: this method will only work if no cycle.. for cycle detecting and then printing see the below method that i submitted in Q "269 Alien dictionary"
-# or we can also modify in this like we can use two visited array like we did in cycle detection Q
+i) Dependency Handling: In topological sorting, if there's an edge u -> v, u must appear before v in the ordering. 
+When we perform DFS, we explore v before finishing u. By adding v to the order before u (since we add on backtracking),
+we ensure that u comes after v in the list. However, since we reverse the list at the end (or use a stack where later additions come first),
+u ends up before v in the final order, satisfying u -> v.
 
-# from collections import defaultdict
-# class Graph:
-#     def __init__(self,n):
-#         self.V= n
-#         self.visited= [False]*n
-#         self.AdjList= defaultdict(list)
+ii) Completion Guarantee: Adding a node after all its descendants are processed means that all nodes that depend on it are already in the order 
+(and will be placed after it upon reversal), ensuring no dependencies are violated.
+
+Why Pre-order Can Fail?
+-> Premature Addition: Adding a node to the order as soon as you start visiting it means you might add u before exploring its descendants. 
+If u has a descendant v that should come after u, but due to the order of DFS traversal,
+v hasn't been added yet, u might end up after v in the final order, violating the u -> v edge.
+
+if you do by finding the indegree like BFS then that will always give the correct ans as that is the basic of tolopogical sort.
+
+Note: this method will only work if no cycle.. for cycle detecting and then printing see the below method that i submitted in Q "269 Alien dictionary"
+or we can also modify in this like we can use two visited array like we did in cycle detection Q.
+"""
+
+from collections import defaultdict
+class Graph:
+    def __init__(self,n):
+        self.V= n
+        self.visited= [False]*n
+        self.AdjList= defaultdict(list)
     
-#     def addEdge(self,u,v):
-#         self.AdjList[u].append(v)
+    def addEdge(self,u,v):
+        self.AdjList[u].append(v)
 
-#     def FindTopoSort(self, adj,src, stack):
-#         self.visited[src]= True
-#         for u in adj[src]:
-#             if not self.visited[u]:
-#                 self.FindTopoSort(adj, u, stack)  
-#         # while traversing back put the node into the stack and node with less no of outorder vertices 
-#         # will be kept first(as it will start traversing back at this node only) so final ans will be the opposite of stack
+    def FindTopoSort(self, adj,src, stack):
+        self.visited[src]= True
+        for u in adj[src]:
+            if not self.visited[u]:
+                self.FindTopoSort(adj, u, stack)  
+        # while traversing back put the node into the stack and node with less no of outorder vertices 
+        # will be kept first(as it will start traversing back at this node only) so final ans will be the opposite of stack
 
-         # node with largest visiting time(or minimum finishing time) is pushed first when there is no further adjacent node is there which has not been visited
-#         stack.append(src)
+         node with largest visiting time(or minimum finishing time) is pushed first when there is no further adjacent node is there which has not been visited
+        stack.append(src)
 
-# you can start with any node, in dfs it doesn't matter in printing topological sort or detecting cycle
-#     def TopoSort(self,n, adj):
-#         stack= []
-#         for i in range(n):
-#             if not self.visited[i]:
-#                 self.FindTopoSort(adj,i,stack)
-#         while stack:  
-#             print(stack.pop(), end=" ")
+    def TopoSort(self,n, adj):
+        stack= []
+        for i in range(n):
+            if not self.visited[i]:
+                self.FindTopoSort(adj,i,stack)
+        while stack:  
+            print(stack.pop(), end=" ")
 
 # test case 1:
-# g= Graph(6)
-# g.addEdge(5,2)
-# g.addEdge(5,0)
-# g.addEdge(2,3)
-# g.addEdge(3,1)
-# g.addEdge(4,0)
-# g.addEdge(4,1)
-# print(g.AdjList)
-# g.TopoSort(6,g.AdjList)
+g= Graph(6)
+g.addEdge(5,2)
+g.addEdge(5,0)
+g.addEdge(2,3)
+g.addEdge(3,1)
+g.addEdge(4,0)
+g.addEdge(4,1)
+print(g.AdjList)
+g.TopoSort(6,g.AdjList)
 
 # test case 2:
-# g= Graph(3)
-# g.addEdge(0,2)
-# g.addEdge(0,1)
-# g.addEdge(1,2)
+g= Graph(3)
+g.addEdge(0,2)
+g.addEdge(0,1)
+g.addEdge(1,2)
 
 
 # another way using dfs: this submitted in Q "269 Alien dictionary"
 
 
 # method 2 using BFS: Kahn's Algorithm
+"""
 # logic: use the concept of indegree as the node with indegree 0 will come at first and
 # node with more indegree will come later and so
 
 # This method can also be used to detect cycle in directed graph
 # just count the no of nodes added in the Q, if there will be cycle then count <n because  at some point 
 # there will not be any node whose indegree will be equal to '0' to put into the Q or call the bfs function.
-# here no need of visited set since we are adding only the node with 'indegree==0'
-
+# here no need of visited set since we are adding only the node with 'indegree==0'.
+"""
 from collections import defaultdict
 class Graph:
     def __init__(self,n):

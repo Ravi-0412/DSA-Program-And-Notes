ae8bdc6fa84ea60393d8ef4e10bd9451857e3323
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-04-09 21:56:08 +0530

    Update and rename 744_smallest_than_target.py to 744. Find Smallest Letter Greater Than Target.py

--- Binary_Search/744. Find Smallest Letter Greater Than Target.py
+++ Binary_Search/744. Find Smallest Letter Greater Than Target.py
@@ -6,17 +6,13 @@ class Solution:
         low= 0
         high= n-1
         while(low<= high):
-            mid= low+ (high- low)//2
-            # if letters[mid]== target:
-            #     return letters[(mid+1)%n]   # this will give the incorrect output 
-                                              # if letters will be repeated then we may get
-                                              # get the same ele as target at next index also
-            if letters[mid]<= target: # in case if equal also then we will search for next greater 
+            mid = low+ (high- low)//2
+            if letters[mid] <= target: # in case if equal also then we will search for next greater 
 
                                       # and for this we have to update low= mid+1
-                low= mid+ 1
+                low = mid+ 1
             else:
-                high= mid-1
+                high = mid-1
         return letters[(low)%n]   # taking modulus to handle the case when ans doesn't exist.
                                   # in this case low will be= n and we have to return the 0th index letter only and 
                                   # if exist then low will give the ans directly.

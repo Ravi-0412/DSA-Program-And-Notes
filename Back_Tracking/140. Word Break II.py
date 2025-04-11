6806abbbe0c6d5545964d9344d9d2aefdb57bd5f
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-04-12 01:14:40 +0530

    Update 140. Word Break II.py

--- Back_Tracking/140. Word Break II.py
+++ Back_Tracking/140. Word Break II.py
@@ -1,15 +1,20 @@
-# logic: just check which substring starting from start is present in 'dict, 
-# if it is present in dict then check for remaining string recursively and keep adding that to ans.
+"""
+logic: just check which substring starting from start is present in 'dict, 
+if it is present in dict then check for remaining string recursively and keep adding that to ans.
 
-# Time Complexity: O(2^n). Because there are 2^n combinations in worst case i.e when every char is present in the dictionary
-# in this case for every char we will have two options either to chose that char or not
-# space: O(n) recursive depth + O(n) for storing ans= O(n)
+Time Complexity: O(2^n). Because there are 2^n combinations in worst case i.e when every char is present in the dictionary
+in this case for every char we will have two options either to chose that char or not
+space: O(n) recursive depth + O(n) for storing ans= O(n)
 
-# Cross Q / follow up 
-# NOte vvvi: "the same word in the dictionary may be reused multiple times in the segmentation.".
-# Due to this only we are able to check if cur substring of 's[i: j+ 1]' matches with any word in 'dict'.
-# If same word of dict is not allowed mutiple times then, we will have to take visited set for dict words(index will work)
-# to check whether the word at that index is already used or not.
+Cross Q / follow up 
+NOte vvvi: "the same word in the dictionary may be reused multiple times in the segmentation.".
+Due to this only we are able to check if cur substring of 's[i: j+ 1]' matches with any word in 'dict'.
+If same word of dict is not allowed mutiple times then, we will have to take visited set for dict words(index will work)
+to check whether the word at that index is already used or not.
+
+Similar Questions:
+1) "139. Word Break"
+"""
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
@@ -26,9 +31,6 @@ class Solution:
                     backtrack(j + 1, sent + [s[i: j + 1]])
         backtrack(0, [])
         return ans
-
-# Similar Questions:
-# 1) "139. Word Break"
 
 # java
 """

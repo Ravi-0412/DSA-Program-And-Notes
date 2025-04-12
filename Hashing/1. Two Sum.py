c7ed3917612a0e0fedcac8b9ba5ea8951e7bcded
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-04-12 11:14:07 +0530

    Update 1. Two Sum.py

--- Hashing/1. Two Sum.py
+++ Hashing/1. Two Sum.py
@@ -1,11 +1,13 @@
-# 1st method: brute force(two nested loops)
+"""
+1st method: brute force(two nested loops)
 
-# 2nd method: using dictionary 
-# time: O(n), space: O(n)
-# logic: for every ele, find the remaining sum then 
-# check the remaining sum is present in the hashamp or not 
-# if remaining sum is already present in the dictionary return its value 
-# if not present then add the current ele into hashmap with its index
+2nd method: using dictionary 
+time: O(n), space: O(n)
+logic: for every ele, find the remaining sum then 
+check the remaining sum is present in the hashamp or not 
+if remaining sum is already present in the dictionary return its value 
+if not present then add the current ele into hashmap with its index
+"""
 def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashmap= {}
         for i in range(len(nums)):
@@ -16,40 +18,39 @@ def twoSum(self, nums: List[int], target: int) -> List[int]:
             else:  # if not present then store the array val as key with index as values
                 hashmap[nums[i]]= i   # since we have to return index so store index as value with remaining sum
 
-# Now if asked to find the no of such pair then keep track of count as well.
-# see 1st method of q: "2006. Count Number of Pairs With Absolute Difference K"
+"""
+Now if asked to find the no of such pair then keep track of count as well.
+see 1st method of q: "2006. Count Number of Pairs With Absolute Difference K"
 
+1) Note vvi: whenever you are asked to check the pair or count the pair for any operation 
+like 'Sum' ,'Subtraction' , 'multiplication' , 'division'
+think of 'Two sum' logic only like how you can get the 2nd number using 1st one. 
+Another number we will get by complement of 1st according to the behaviour of operator.
 
-# 1) Note vvi: whenever you are asked to check the pair or count the pair for any operation 
-# like 'Sum' ,'Subtraction' , 'multiplication' , 'division'
-# think of 'Two sum' logic only like how you can get the 2nd number using 1st one. 
-# Another number we will get by complement of 1st according to the behaviour of operator.
+Note: In case of pair count always store the frequency.
+You can traverse and check for pair in parallel.
 
-# Note: In case of pair count always store the frequency.
-# You can traverse and check for pair in parallel.
+2) Note: Store 'what you need to find(for pair i.e by help of which you will get one of ans) as key' and 
+'what you need in ans(or due to which you can get the ans after some operation) as value' in hashmap.
+e.g: "in Two sum Q", we want to check remaining sum(another number) so kept this as 'key' and
+in ans we wanted the index so kept 'index' as value.
 
-# 2) Note: Store 'what you need to find(for pair i.e by help of which you will get one of ans) as key' and 
-# 'what you need in ans(or due to which you can get the ans after some operation) as value' in hashmap.
-# e.g: "in Two sum Q", we want to check remaining sum(another number) so kept this as 'key' and
-# in ans we wanted the index so kept 'index' as value.
+It will help in lot of Q like : "2006. Count Number of Pairs With Absolute Difference K",
+ "1679. Max Number of K-Sum Pairs", "2183. Count Array Pairs Divisible by K" etc...
 
-# It will help in lot of Q like : "2006. Count Number of Pairs With Absolute Difference K",
-#  "1679. Max Number of K-Sum Pairs", "2183. Count Array Pairs Divisible by K" etc...
+3) Note: whenever you have to find the subarray length or count of subarray with given sum 'k' or
+ given xor 'k' or related to nay operation. => Apply two sum logic.
+since "+" has complement "-" i.e given a sum say 'target' then say till any index 'j' totalSum= curSum 
+then if we can find any ele 'x' at index 'i' having such that x= curSum - target,  
+then we can get  the target by doing sum of all ele from index 'i+1' to 'j'.
 
-# 3) Note: whenever you have to find the subarray length or count of subarray with given sum 'k' or
-#  given xor 'k' or related to nay operation. => Apply two sum logic.
-# since "+" has complement "-" i.e given a sum say 'target' then say till any index 'j' totalSum= curSum 
-# then if we can find any ele 'x' at index 'i' having such that x= curSum - target,  
-# then we can get  the target by doing sum of all ele from index 'i+1' to 'j'.
+same way xor work because of its property : if res= a^b then res^a= b and res^b= a.
+replace res->curSum , b= target then if we will find 'a' such that a= res ^ b then means we can get the target 'b'.
 
-# same way xor work because of its property : if res= a^b then res^a= b and res^b= a.
-# replace res->curSum , b= target then if we will find 'a' such that a= res ^ b then means we can get the target 'b'.
-
-# e.g : "523. Continuous Subarray Sum" , "974. Subarray Sums Divisible by K" , 
-# "Longest Sub-Array with Sum K" ,"Smallest Subarray with Sum K",
-# "560. Subarray Sum Equals K", "Count Subarrays with Given XOR"  etc....
-
-
+e.g : "523. Continuous Subarray Sum" , "974. Subarray Sums Divisible by K" , 
+"Longest Sub-Array with Sum K" ,"Smallest Subarray with Sum K",
+"560. Subarray Sum Equals K", "Count Subarrays with Given XOR"  etc....
+"""
 
 # Extension:
 # q)  If had asked count no of such pair then,
@@ -120,7 +121,9 @@ class Solution {
 }
 """
 
-# Related Question:
-# 1) 2006. Count Number of Pairs With Absolute Difference K
-# 2) Print all unique pairs with given sum
-# 3) 167. Two Sum II - Input Array Is Sorted
+"""
+Related Question:
+1) 2006. Count Number of Pairs With Absolute Difference K
+2) Print all unique pairs with given sum
+3) 167. Two Sum II - Input Array Is Sorted
+"""

8d9715b66fc9c157e561be3875d35c210db6716c
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-04-12 09:15:43 +0530

    Update Job Sequencing Problem.py

--- Greedy/Job Sequencing Problem.py
+++ Greedy/Job Sequencing Problem.py
@@ -1,32 +1,34 @@
-# NOTE: job is given as node not in form of array so we can't sort directly.
+"""
+NOTE: job is given as node not in form of array so we can't sort directly.
 
-# Note: Don't blindly start solving the problem, first:
-# 1) study & understand problem properly.
-# 2) See the input and output format 
-# 3) VVI: Check if input is given in '0- based' indexing or '1-based' indexing.
-# 4) Check the constraint 
+Note: Don't blindly start solving the problem, first:
+1) study & understand problem properly.
+2) See the input and output format 
+3) VVI: Check if input is given in '0- based' indexing or '1-based' indexing.
+4) Check the constraint 
 
-# Then only start to solve problem.
+Then only start to solve problem.
 
 
-# Logic: we will try to include job with maximum profit first.
-# But to get maximum ans, we will try to include other job as well.
+Logic: we will try to include job with maximum profit first.
+But to get maximum ans, we will try to include other job as well.
 
-# So we will put job having maximum profit as far as possible.
-# steps:
-# 1) create array of size = max_deadline_among_all_jobs
-# 2) Sort jobs based on profit in descending order
-# 3) Take the job one by one and place it at appropriate empty slot.
-# Place as far as possible so that we can include other job also.
-# For this start checking for empty slot from 'job_deadline' in reverse direction.
+So we will put job having maximum profit as far as possible.
+steps:
+1) create array of size = max_deadline_among_all_jobs
+2) Sort jobs based on profit in descending order
+3) Take the job one by one and place it at appropriate empty slot.
+Place as far as possible so that we can include other job also.
+For this start checking for empty slot from 'job_deadline' in reverse direction.
 
-# 4) Once you find any empty slot then add the profit of that job in ans and 
-# mark that empty slot as occupied.
-# 5) Return ans
+4) Once you find any empty slot then add the profit of that job in ans and 
+mark that empty slot as occupied.
+5) Return ans
 
-# Submitted on gfg.
+Submitted on gfg.
 
-# time: O(n^2)
+time: O(n^2)
+"""
 
 '''
 class Job:
@@ -60,10 +62,53 @@ class Solution:
                     break   # after you fina any empty slot
         return [jobs_done, maxProfit]
 
+# Java
+"""
+import java.util.Arrays;
+
+class Job {
+    int id;
+    int profit;
+    int deadline;
+
+    Job(int id, int deadline, int profit) {
+        this.id = id;
+        this.deadline = deadline;
+        this.profit = profit;
+    }
+}
+
+class Solution {
+    public int[] JobScheduling(Job[] Jobs_given, int n) {
+        Arrays.sort(Jobs_given, (a, b) -> b.profit - a.profit);
+
+        int maxDeadline = 0;
+        for (Job job : Jobs_given) {
+            maxDeadline = Math.max(maxDeadline, job.deadline);
+        }
+
+        int[] position = new int[maxDeadline + 1];
+        Arrays.fill(position, -1);
+
+        int jobsDone = 0, maxProfit = 0;
+
+        for (Job job : Jobs_given) {
+            for (int j = job.deadline; j > 0; j--) {
+                if (position[j] == -1) {
+                    position[j] = job.id;
+                    maxProfit += job.profit;
+                    jobsDone++;
+                    break;
+                }
+            }
+        }
+
+        return new int[]{jobsDone, maxProfit};
+    }
+}
+"""
 
 # If jobs are given in form of array like Jobs[i] = [job_id, deadline, profit]
-# 
-
 
 def JobScheduling(Jobs,n):
     Jobs.sort(key= lambda x : x[2], reverse= True)
@@ -92,6 +137,44 @@ def JobScheduling(Jobs,n):
 Jobs= [(1,5,200),(2,3,180),(3,3,190),(4,2,300),(5,4,120),(6,2,100)]
 n= len(Jobs)
 print(JobScheduling(Jobs,n))
+
+# Java
+"""
+import java.util.Arrays;
+
+class Solution {
+    public int[] JobScheduling(int[][] Jobs, int n) {
+        Arrays.sort(Jobs, (a, b) -> b[2] - a[2]);
+
+        int maxDeadline = 0;
+        for (int[] job : Jobs) {
+            maxDeadline = Math.max(maxDeadline, job[1]);
+        }
+
+        int[] position = new int[maxDeadline];
+        Arrays.fill(position, -1);
+
+        int jobsDone = 0, maxProfit = 0;
+
+        for (int[] job : Jobs) {
+            int id = job[0];
+            int deadline = job[1];
+            int profit = job[2];
+
+            for (int j = deadline - 1; j >= 0; j--) {
+                if (position[j] == -1) {
+                    position[j] = id;
+                    maxProfit += profit;
+                    jobsDone++;
+                    break;
+                }
+            }
+        }
+
+        return new int[]{jobsDone, maxProfit};
+    }
+}
+"""
 
 # python used function and methods
 # https://www.geeksforgeeks.org/python-list-sort-method/

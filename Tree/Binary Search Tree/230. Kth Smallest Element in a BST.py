229fd0558993ed8014ebd600c29d8e0beb563dc4
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-04-26 07:57:56 +0530

    Update 230. Kth Smallest Element in a BST.py

--- Tree/Binary Search Tree/230. Kth Smallest Element in a BST.py
+++ Tree/Binary Search Tree/230. Kth Smallest Element in a BST.py
@@ -1,30 +1,32 @@
-# method 1: do any traversal and then sort the ans and then return the kth ele from start
-# time: O(n*logn), space: O(n)
+"""
+method 1: do any traversal and then sort the ans and then return the kth ele from start
+time: O(n*logn), space: O(n)
 
-# method 2: just find the inorder traversal of the tree and return the kth element from the start
-# inorder always give the ans in sorted form for BST
-# time: O(n), space: O(n) for ans + recursion depth
+method 2: just find the inorder traversal of the tree and return the kth element from the start
+inorder always give the ans in sorted form for BST
+time: O(n), space: O(n) for ans + recursion depth
 
-# method 3: rather than storing the ans in any array just keep a count for method 2
-# and increment the count when you add any ele to the ans 
-# and when count reaches 'k' just print the value of that node
-# time: O(n), space: O(n) recursion depth
+method 3: rather than storing the ans in any array just keep a count for method 2
+and increment the count when you add any ele to the ans 
+and when count reaches 'k' just print the value of that node
+time: O(n), space: O(n) recursion depth
 
-# to avoid the space complexity in method 3
-# use the morris inorder traversal
+Note: to avoid the space complexity in method 3
+use the morris inorder traversal
 
 
-# Note: All above method is taking O(n) space
+Note: All above method is taking O(n) space
 
-# Method 4: 
+Method 4: 
 
-# Optimsing to O(1) space
+Optimsing to O(1) space
 
-# Logic: Smallest node will be the leftmost node.
-# so once you find '.left' of any node = None then that will be the smallest node as of now.
-# And to get the next smaller node , move to its right.
+Logic: Smallest node will be the leftmost node.
+so once you find '.left' of any node = None then that will be the smallest node as of now.
+And to get the next smaller node , move to its right.
 
-# If it's right is also None then it will do backtrack function call to previous node and calculate in same way.
+If it's right is also None then it will do backtrack function call to previous node and calculate in same way.
+"""
 
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
@@ -47,7 +49,7 @@ class Solution:
 
 # My mistake
 # Note: if we do taking 'k' in function call only then we will get wrong ans
-# because after backtarck value of 'k' will be won't change for that function.
+# because after backtrack value of 'k' will be won't change for that function.
 # but it should change globally like above.
 
 class Solution:

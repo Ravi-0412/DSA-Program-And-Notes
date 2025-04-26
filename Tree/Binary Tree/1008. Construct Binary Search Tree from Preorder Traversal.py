222b357bc1b11b4cc9f287a54ed6add3ba1e65aa
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-04-26 13:19:08 +0530

    Update 1008. Construct Binary Search Tree from Preorder Traversal.py

--- Tree/Binary Tree/1008. Construct Binary Search Tree from Preorder Traversal.py
+++ Tree/Binary Tree/1008. Construct Binary Search Tree from Preorder Traversal.py
@@ -5,21 +5,19 @@
 
 class Solution:
     def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
-        inorder= sorted(preorder)
+        inorder = sorted(preorder)
         return self.buildTree(preorder, inorder)
     
     def buildTree(self, preorder, inorder):
         if not inorder: # or preorder
             return None
-        root= TreeNode(preorder[0])   # this will be the root
-
+        root = TreeNode(preorder[0])   # this will be the root
         # find the index of 1st ele of prerorder in inorder to check all nodes will go the left of parent and right of parent.
-        ind= inorder.index(preorder[0])
+        ind = inorder.index(preorder[0])
         # all the ele from index '1' till 'ind' will come to the left in preorder(1st ele already included.) and all the ele before the indx in inorder will come to the left subtree(ind ele already included)
-        root.left=  self.buildTree(preorder[1:ind+1], inorder[:ind])
+        root.left =  self.buildTree(preorder[1:ind+1], inorder[:ind])
         # all the ele after the indx will come to right of both preorder and inorder.
-        root.right= self.buildTree(preorder[ind+1 :], inorder[ind+1 :])                
-
+        root.right = self.buildTree(preorder[ind+1 :], inorder[ind+1 :])                
         return root
 
 # method 2: simply sort and then apply the "convert the given sorted array into Balanced BST".
@@ -28,33 +26,81 @@ class Solution:
 # method 3: Take each ele in preorder and apply the normal method to form BST i.e insert node in BST one by one.
 # time: O(n*logn).
 
-# Method 4:
+# Method 4: Simplest one 
+"""
+Note: For any node all smaller element will go on left side and all greater element will go on right side.
+Time = O(n), space : O(1)
+"""
+
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
+        def build(preorder):
+            if not preorder:
+                return None
+
+            root = TreeNode(preorder[0])
+            # Split into left and right parts
+            i = 1
+            while i < len(preorder) and preorder[i] < root.val:
+                i += 1
+
+            root.left = build(preorder[1:i])   # Left elements are smaller
+            root.right = build(preorder[i:])   # Right elements are greater
+            return root
+
+        return build(preorder)
+
+# Java
+"""
+class Solution {
+    public TreeNode bstFromPreorder(int[] preorder) {
+        return build(preorder, 0, preorder.length);
+    }
+
+    private TreeNode build(int[] preorder, int start, int end) {
+        if (start >= end) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(preorder[start]);
+        int i = start + 1;
+        
+        // Find the index where right subtree starts (greater than root)
+        while (i < end && preorder[i] < root.val) {
+            i++;
+        }
+
+        root.left = build(preorder, start + 1, i);  // Elements smaller than root
+        root.right = build(preorder, i, end);       // Elements greater than root
+        return root;
+    }
+}
+"""
 
 """
-Idea is simple: 
+Idea is simple but above one is more simpler and best.
 1) First item in preorder list is the root to be considered.
 2) For next item in preorder list, there are 2 cases to consider:
 2.a) If value is less than last item in stack, it is the left child of last item.
-2.b) If value is greater than last item in stack, pop it.
+2.b) If value is greater than last item in stack, pop it. It will be right child of last item.
       The last popped item will be the parent and the item will be the right child of the parent.
-"""
 
-"""
+
 How to think of this logic?
-# Q. How to come up with this logic?
-# Ans: Just given the preorder, draw the BST on paper and analyse how you are putting the nodes and 
-# which Data Structure we can use to get BST directly from preorder.
+Q. How to come up with this logic?
+Ans: Just given the preorder, draw the BST on paper and analyse how you are putting the nodes and 
+which Data Structure we can use to get BST directly from preorder.
 
-# Q. why we are directly adding when num is samller and poping before adding when num is greater?
-# Ans: if smaller means that must be the left child of the just previous ele in stack , 
-# Because we are doing acc to the preorder and in preorder we will move to left after root and 
-# it is BST so smaller ele will be on left.(root, left right,)
-# vvi: until we find any ele greater than top of stack, all those num will be go as left child only(like skew tree).
-# And we will find any ele greater then we will search for the node to which 'num' will be the right child.  
-# (now direction of tree will change).
-# i.e we will find the last smaller ele from the current num. 'num' will be the right child of that ele.
+Q. why we are directly adding when num is smaller and poping before adding when num is greater?
+Ans: if smaller means that must be the left child of the just previous ele in stack , 
+Because we are doing acc to the preorder and in preorder we will move to left after root and 
+it is BST so smaller ele will be on left.(root, left right,)
+vvi: until we find any ele greater than top of stack, all those num will be go as left child only(like skew tree).
+And we will find any ele greater then we will search for the node to which 'num' will be the right child.  
+(now direction of tree will change).
+i.e we will find the last smaller ele from the current num. 'num' will be the right child of that ele.
 
-# That's why we are using stack.
+That's why we are using stack.
 """
 
 # Time = space = O(n)
@@ -105,7 +151,7 @@ class Solution {
 """
 
 
-# Method 5: Optimising space to O(1)
+# Method 5:
 # Time: O(n), space = O(1) 
 
 class Solution:

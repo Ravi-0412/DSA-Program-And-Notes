80574d5bc9509b0447dc2a0822c5810503afee8a
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-04-30 14:02:31 +0530

    Update 19. Remove Nth Node From End of List.py

--- LinkList/19. Remove Nth Node From End of List.py
+++ LinkList/19. Remove Nth Node From End of List.py
@@ -53,29 +53,6 @@ class Solution:
         second.next= second.next.next
         return dummy.next  
 
-# Java
-"""
-class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode first = dummy;
-        ListNode second = dummy;
-
-        for (int i = 0; i < n; i++) {
-            first = first.next;
-        }
-
-        while (first.next != null) {
-            first = first.next;
-            second = second.next;
-        }
-
-        second.next = second.next.next;
-        return dummy.next;
-    }
-}
-"""
 
 # Method 3: just same logic as method 2
 # in one pass.

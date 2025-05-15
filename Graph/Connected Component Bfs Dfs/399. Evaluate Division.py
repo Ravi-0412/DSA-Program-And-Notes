bba52788c0984e6d6609ad940366e2e1bbbe35ec
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-05-15 09:35:15 +0530

    Update 399. Evaluate Division.py

--- Graph/Connected Component Bfs Dfs/399. Evaluate Division.py
+++ Graph/Connected Component Bfs Dfs/399. Evaluate Division.py
@@ -1,27 +1,29 @@
-# logic: just simple bfs to find the cost from src to destination.
-# Difficult part to think how we can do by graph.
+"""
+logic: just simple bfs to find the cost from src to destination.
+Difficult part to think how we can do by graph.
 
-# expalanation:
-# Given:
-# a/b = 2.0, b/c = 3.0
-# We can build a directed graph:
-# a --> 2.0 --> b -- 3.0 --> c
-# If we were asked to find a/c, we have:
-# a/c = a/b * b/c = 2.0 * 3.0
-# In the graph, it is the product of costs of edges.
+expalanation:
+Given:
+a/b = 2.0, b/c = 3.0
+We can build a directed graph:
+a --> 2.0 --> b -- 3.0 --> c
+If we were asked to find a/c, we have:
+a/c = a/b * b/c = 2.0 * 3.0
+In the graph, it is the product of costs of edges.
 
-# Do notice that, 2 edges need to added into the graph with one given equation,
-# because with a/b we also get result of b/a, which is the reciprocal of a/b.
-# in short using reciprocal we can also get ans .
+Do notice that, 2 edges need to added into the graph with one given equation,
+because with a/b we also get result of b/a, which is the reciprocal of a/b.
+in short using reciprocal we can also get ans .
 
-# e.g: If asked to find 'c/a' then reciprocal will only help.
-# c -- 0.333 --> b -- 0.5 --> a
+e.g: If asked to find 'c/a' then reciprocal will only help.
+c -- 0.333 --> b -- 0.5 --> a
 
-# how to do?
-# FOr each query [a, b], run a bfs starting from 'a' and keep multiplying the edges that is adjacent 
-# And once you reach 'b', return ans. (a and b can be equal also)
+how to do?
+FOr each query [a, b], run a bfs starting from 'a' and keep multiplying the edges that is adjacent 
+And once you reach 'b', return ans. (a and b can be equal also)
 
-# time: n * (E+ V), n= len(queries)
+time: n * (E+ V), n= len(queries)
+"""
 
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
@@ -55,6 +57,87 @@ class Solution:
             curAns= bfs(q[0], q[1])
             ans.append(curAns)
         return ans
+
+# Java
+"""
+import java.util.*;
+
+public class Solution {
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        // Adjacency list to represent the graph
+        Map<String, List<Pair>> graph = new HashMap<>();
+
+        // Build the graph: for each equation a / b = val, also store b / a = 1/val
+        for (int i = 0; i < equations.size(); i++) {
+            List<String> eq = equations.get(i);
+            String a = eq.get(0), b = eq.get(1);
+            double val = values[i];
+
+            graph.putIfAbsent(a, new ArrayList<>());
+            graph.putIfAbsent(b, new ArrayList<>());
+
+            graph.get(a).add(new Pair(b, val));       // a -> b with weight val
+            graph.get(b).add(new Pair(a, 1.0 / val)); // b -> a with weight 1/val
+        }
+
+        // Prepare result array
+        double[] results = new double[queries.size()];
+
+        // Process each query using BFS
+        for (int i = 0; i < queries.size(); i++) {
+            String src = queries.get(i).get(0);
+            String dest = queries.get(i).get(1);
+            results[i] = bfs(src, dest, graph);
+        }
+
+        return results;
+    }
+
+    // Helper function to perform BFS
+    private double bfs(String src, String dest, Map<String, List<Pair>> graph) {
+        if (!graph.containsKey(src) || !graph.containsKey(dest)) {
+            return -1.0;
+        }
+
+        Queue<Pair> queue = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+
+        queue.offer(new Pair(src, 1.0));
+        visited.add(src);
+
+        while (!queue.isEmpty()) {
+            Pair current = queue.poll();
+            String node = current.variable;
+            double valueSoFar = current.value;
+
+            if (node.equals(dest)) {
+                return valueSoFar;
+            }
+
+            for (Pair neighbor : graph.get(node)) {
+                if (!visited.contains(neighbor.variable)) {
+                    visited.add(neighbor.variable);
+                    queue.offer(new Pair(neighbor.variable, valueSoFar * neighbor.value));
+                }
+            }
+        }
+
+        // If no path was found
+        return -1.0;
+    }
+
+    // Helper class to store pairs of (variable, value)
+    static class Pair {
+        String variable;
+        double value;
+
+        Pair(String variable, double value) {
+            this.variable = variable;
+            this.value = value;
+        }
+    }
+}
+"""
 
 
 # Later do by dfs

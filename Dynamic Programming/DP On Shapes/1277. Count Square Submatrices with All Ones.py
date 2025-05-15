72543099433787e86b8856af17f11f39f7bd088d
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-05-16 00:35:33 +0530

    Update 1277. Count Square Submatrices with All Ones.py

--- Dynamic Programming/DP On Shapes/1277. Count Square Submatrices with All Ones.py
+++ Dynamic Programming/DP On Shapes/1277. Count Square Submatrices with All Ones.py
@@ -1,18 +1,21 @@
-# Logic: Har ek cell (r, c) se check kar rhe ki maximum kitne side ka square form kar sakte h.
-# Iske liye hmko 3 direction ka length ka min lena hoga i.e (diagonally upper-left, upper_cell, left_cell) => 
-# min(r-1, c-1), (r, c- 1), (r-1, c)) and current cell ka '1' add karna hoga agar cur cell (r, c) ka value '1' h tb.
+"""
+Logic: Har ek cell (r, c) se check kar rhe ki maximum kitne side ka square form kar sakte h.
+Iske liye hmko 3 direction ka length ka min lena hoga i.e (diagonally upper-left, upper_cell, left_cell) => 
+min(r-1, c-1), (r, c- 1), (r-1, c)) and current cell ka '1' add karna hoga agar cur cell (r, c) ka value '1' h tb.
 
-# Let cur cell se ans = 3 then means including this cell we can form squre of side of length '3'.
-# But yahan number puch rha square ka then ye side '1' ka square  , side '2' and side '3' ka square form kar sakta h adjacent cell
-# se combine hoke. Isliye jo side aayega wahi mera count hoga us cell ke liye.
+Let cur cell se ans = 3 then means including this cell we can form squre of side of length '3'.
+But yahan number puch rha square ka then ye side '1' ka square  , side '2' and side '3' ka square form kar sakta h adjacent cell
+se combine hoke. Isliye jo side aayega wahi mera count hoga(new squares) us cell ke liye.
 
-# Isse hm direct Tabulation DP likh sakte h, Top - Down . See method 4.
+Isse hm direct Tabulation DP likh sakte h, Top - Down . See method 4.
+"""
 
-
-# # method 1: recursive.
-# note: jyada dimag mat lagao recursion me , sb sahi logic socho or likh do.
-# Yahan (0, 0) se ja rhe isliye hmko check karne ka direction change karna hoga.
-# i.e min(down, right, lower_right diagonal).
+"""
+method 1: recursive.
+note: jyada dimag mat lagao recursion me , bs sahi logic socho or likh do.
+Yahan (0, 0) se ja rhe isliye hmko check karne ka direction change karna hoga.
+i.e min(down, right, lower_right diagonal).
+"""
 
 class Solution:
     def countSquares(self, matrix: List[List[int]]) -> int:
@@ -138,18 +141,20 @@ class Solution {
 """
 
 # method 4: Tabulation (top Down)
-# time: O(m*n)= space
-# logic is totally same as above.
+"""
+time: O(m*n)= space
+logic is totally same as above.
 
-# logic: just find the no of square matrix whose lower right corner is matrix[i][j].
-# for 1st row and 1st col, ans will be matrix value only.
+logic: just find the no of square matrix whose lower right corner is matrix[i][j].
+for 1st row and 1st col, ans will be matrix value only.
 
-# why taking min of all three?
-# since we are considering square so from all the three sides it should be a valid square then,adding the current ele will make valid square only.
-# if we not take minimum of all three then it can't be a square, it can be a rectangle also.
+why taking min of all three?
+since we are considering square so from all the three sides it should be a valid square then,adding the current ele will make valid square only.
+if we not take minimum of all three then it can't be a square, it can be a rectangle also.
 
-# why counting the square ending with bottom right corner not starting with?
-# because we only need to compare the three values but in later one we will have to find the no of square recursively.
+why counting the square ending with bottom right corner not starting with?
+because we only need to compare the three values but in later one we will have to find the no of square recursively.
+"""
 
 
 class Solution:
@@ -182,8 +187,8 @@ class Solution:
         for r in range(1, row + 1):
             for c in range(1, col + 1):
                 if matrix[r-1][c-1] == 1:
-                    dp[r][c]= 1+ min(dp[r-1][c], dp[r][c-1], dp[r-1][c-1])   # up, left, upper-left diagonal
-                    ans+= dp[r][c]
+                    dp[r][c]= 1+ min(dp[r-1][c], dp[r][c-1], dp[r-1][c-1])   # side length = min(up, left, upper-left diagonal)
+                    ans += dp[r][c]  # new formed square using current cell = side_length only
         return ans
 
 # Java

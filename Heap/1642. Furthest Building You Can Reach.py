87b6518650454e70533d0fba8206fddb09ac8777
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-05-17 11:30:56 +0530

    Update 1642. Furthest Building You Can Reach.py

--- Heap/1642. Furthest Building You Can Reach.py
+++ Heap/1642. Furthest Building You Can Reach.py
@@ -1,10 +1,37 @@
-# Logic
 """
-it's better to use ladder for larger difference and bricks for smaller difference.
+Note: First understand Why Dp won't work here?
+Two points to consider here:
+1. If the next building is on lower or equal height I can move on.
+
+2. If the next building is higher than I have two choices 
+I can use ladder or I can use no. of bricks of that height difference.  => From this we can think of DP.
+But it will be 3D DP(index * bricks * ladder) and size will be very large that you will get Memory Limit Exceeded.
+
+Here it's not like I have choice either I can pick ladder or brick, I also need to make sure I reach the maximum index.
+
+"""
+
+
+# Logic: Using Heap
+"""
+it's better to use ladder for larger difference and bricks for smaller difference you will waste too many bricks.
+
+So here we know that we will use ladders for the highest climb and bricks for the remaining.
 
 first try to use ladder only but in case we don't have sufficient ladder then we will use bricks.
-But in that case difference should be smaller.
-For this we will maintain a MinHeap.
+But in that case difference should be smaller. For this we will maintain a MinHeap.
+
+Q) How will you manage this, like for the highest climb you are using ladders and for other you are using bricks.
+
+1) First of all as we climb one by one, we will use ladders till we are out of ladders.
+2) Now when we have consumed our all ladders, now it's our turn to use bricks. Now let's say previously we have use 
+ladders to climb [4, 10, 6] and now we have to climb 5, so now I know that for 5 I should use ladder, 
+and the previously I have used ladder for 4 I will use brick there.
+3) So now ladders will be used to climb [10, 6, 5] and bricks for [4].
+4) Now let's say I have to climb 2, then obviously I can't use ladders because they are already being used to climb high. 
+SO I will use bricks here. But let's say I have one 1 brick left then I won't be able to climb further.
+We got to know that if the climb is lower or equal to the climb made by ladders then we can use brick.
+but if we found high climb where previously we have used ladders for the lower climb, I will use ladders here and bricks there.
 
 We will add element in minHeap if difference(heights[i+ 1] - heights[i]) will be > 0.
 Note: len(heap) will the number of ladder we have utilised.

cd00c49ca9422aab7824bab6ab76e1980b888a27
satwika <chitrasatwika@gmail.com>
2025-06-01 12:20:52 +0530

    meeting rooms 1

--- Greedy/252· Meeting Rooms.py
+++ Greedy/252· Meeting Rooms.py
@@ -1,36 +1,124 @@
-# submitted on Neetcode.io
+# Intution first, Codes next (in Python,Java,C++) and Time complexity at the last. 
 
-# if starting time of any meeting is < ending time of previous ongoing meeting then person can't atttend all meetings.
-# And we can only compare if we know the timing of prevoius meeeting.
-# And to know which meeting is going before we need to sort the intervals based on starting time.
+# Intution :
+
+# Our goal is to make the person attend all the meetings - If he can do so, we need to return True, else False.
+# Initially, all the meeting timings are given randomly, so we need to arrange them, according to their times, in such a way that the person can attend all the meetings.
+# So, let's sort the meetings array based on the start time of each meeting.
+
+# EDGE CASE :
+# If the meetings array just contains of one meeting (n==1), then we will return True without any further checks.
+
+# We will check if the start time of the current meeting is more than the end time of the previous meeting. If yes, we will proceed, else we will return False. 
+# To check for the previous meeting, we will start iterating in the meetings array from 1st index, instead of the 0th.
+
+# If we pass complete the iteration without returning False, then we will return True.
+
+# CODES : 
+
+# PYTHON
 
 
-# time: O(n*logn)
 class Solution:
-    def can_attend_meetings(self, intervals: List[Interval]) -> bool:
-        intervals.sort(key= lambda i: i.start)  # sorting based on starting point
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda i: i.start)
+
         for i in range(1, len(intervals)):
-            end1=   intervals[i-1].end
-            start2= intervals[i].start
-            if start2 < end1:
+            i1 = intervals[i - 1]
+            i2 = intervals[i]
+
+            if i1.end > i2.start:
                 return False
         return True
+    
 
-# Java
-"""
-public class Solution {
-    public boolean canAttendMeetings(List<Interval> intervals) {
-        // Sort intervals by their start time using a lambda function
-        Collections.sort(intervals, (a, b) -> a.start - b.start);
 
-        // Check for overlapping intervals
-        for (int i = 1; i < intervals.size(); i++) {
-            if (intervals.get(i).start < intervals.get(i - 1).end) {
-                return false;  // If an overlap is found, return false
-            }
-        }
 
-        return true;  // No overlaps found, return true
-    }
-}
-"""
+
+# JAVA 
+
+
+
+
+# import java.util.*;
+
+# class Interval {
+#     int start;
+#     int end;
+
+#     Interval() {}
+#     Interval(int s, int e) {
+#         start = s;
+#         end = e;
+#     }
+# }
+
+# class Solution {
+#     public boolean canAttendMeetings(Interval[] intervals) {
+#         Arrays.sort(intervals, (a, b) -> a.start - b.start);
+
+#         for (int i = 1; i < intervals.length; i++) {
+#             if (intervals[i - 1].end > intervals[i].start) {
+#                 return false;
+#             }
+#         }
+#         return true;
+#     }
+# }
+
+
+
+
+
+# C++
+
+
+
+
+
+#include <vector>
+#include <algorithm>
+
+# using namespace std;
+
+# struct Interval {
+#     int start;
+#     int end;
+#     Interval() {}
+#     Interval(int s, int e) : start(s), end(e) {}
+# };
+
+# class Solution {
+# public:
+#     bool canAttendMeetings(vector<Interval>& intervals) {
+#         sort(intervals.begin(), intervals.end(), [](const Interval &a, const Interval &b) {
+#             return a.start < b.start;
+#         });
+
+#         for (int i = 1; i < intervals.size(); i++) {
+#             if (intervals[i - 1].end > intervals[i].start) {
+#                 return false;
+#             }
+#         }
+#         return true;
+#     }
+# };
+
+
+
+
+
+# TIME COMPLEXITY : 
+
+# -> Sorting : 0(n log n)
+# -> Iteration : 0(n)
+
+# Overall : 0(n log n)
+
+
+
+
+# SPACE COMPLEXITY :
+
+# O(1) (if sorting is in-place)
+

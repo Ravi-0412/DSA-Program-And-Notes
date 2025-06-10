fc66594988dcdce315080bb5e470af9fb1799ebc
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-06-11 00:39:39 +0530

    Update 41. First Missing Positive.py

--- Array/41. First Missing Positive.py
+++ Array/41. First Missing Positive.py
@@ -205,20 +205,3 @@ public:
     }
 };
 """
-
-
-# Try by this approach also in python.
-# https://leetcode.com/problems/first-missing-positive/solutions/17071/my-short-c-solution-o-1-space-and-o-n-time/
-# Tried but not working.
-
-class Solution:
-    def firstMissingPositive(self, nums: List[int]) -> int:
-        n= len(nums)
-        for i in range(n):
-            while nums[i] > 0 and nums[i] <= n and nums[nums[i] - 1] != nums[i]:
-                nums[i], nums[nums[i] -1]= nums[nums[i] -1], nums[i]
-        
-        for i in range(n):
-            if nums[i]!= i+1:
-                return i +1
-        return n + 1

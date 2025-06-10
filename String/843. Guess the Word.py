5afc039bc2bbfe216ae5ee66d1f389b0c740b32f
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-06-11 00:45:54 +0530

    Update 843. Guess the Word.py

--- String/843. Guess the Word.py
+++ String/843. Guess the Word.py
@@ -1,51 +1,56 @@
-# Logic: We have to narrow the candidates after each time we call master.guess().
-# We have to find some strategy to 'get a similarity score between possible secret and a guess'.
+"""
+Logic: We have to narrow the candidates after each time we call master.guess().
+We have to find some strategy to 'get a similarity score between possible secret and a guess'.
 
-# Note : 1. If word has score x > 0, only those words that share exactly x characters with word can be the solution.
-# (Not word with lesser score nor with greater score).
+Note : 1. If word has score x > 0, only those words that share exactly x characters with word can be the solution.
+(Not word with lesser score nor with greater score).
 
-# Why?
-# e.g. suppose guess("xyz") = 1.
+Why?
+e.g. suppose guess("xyz") = 1.
 
-# a) "abc" cannot be the solution because it shares 0 letters with "xyz". 
-# "xyz" has a score of 1 so "abc" must have at least 1 letter wrong to be the possible secret, so cannot be the solution.
+a) "abc" cannot be the solution because it shares 0 letters with "xyz". 
+"xyz" has a score of 1 so "abc" must have at least 1 letter wrong to be the possible secret, so cannot be the solution.
 
-# b ) "zxy" cannot be the solution because it shares 0 letters (in the right place) with "xyz", so it must have at least one letter wrong.
+b ) "zxy" cannot be the solution because it shares 0 letters (in the right place) with "xyz", so it must have at least one letter wrong.
 
-# c) "ayz" cannot be the solution because it shares 2 letters with "xyz". If "ayz" were the solution then "xyz" would have a score of 2.
+c) "ayz" cannot be the solution because it shares 2 letters with "xyz". If "ayz" were the solution then "xyz" would have a score of 2.
 
-# d ) "abz" can be the solution because it shares 1 letter with "xyz".
+d ) "abz" can be the solution because it shares 1 letter with "xyz".
 
-# Using above logic we can eliminate the possible secrets words.
+Using above logic we can eliminate the possible secrets words.
 
-# But this on its own is not sufficient. We need to shrink the list faster for the big test cases.
-# For this we have to choose our guess based on some strategy to eliminate as max as possible to reach the secret fast.
+But this on its own is not sufficient. We need to shrink the list faster for the big test cases.
+For this we have to choose our guess based on some strategy to eliminate as max as possible to reach the secret fast.
 
 
-# How can be do this?
+How can be do this?
 
-# Method 1: Use the most overlapping words
-# Why?
-# we want to find a word that overlaps most with other words so that we could eliminate all words that might have 0 matches with the guess word, 
-# this would help us to reach the secret asap.
+Intition: Use the most overlapping words
+Why?
+we want to find a word that overlaps most with other words so that we could eliminate all words that might have 0 matches with the guess word, 
+this would help us to reach the secret asap.
 
-# 1) choose the most overlapping word and check if it is secret word.
-# why to do by this ?
-# Ans: for most_overlapping words say 'w', guess function will return count of matched char  say 'x' then,
-# Next possible word must have matches == 'x' with most_overlapping words.
-# From this we can eliminate a lot of words
+1) choose the most overlapping word and check if it is secret word.
+why to do by this ?
+Ans: for most_overlapping words say 'w', guess function will return count of matched char  say 'x' then,
+Next possible word must have matches == 'x' with most_overlapping words.
+From this we can eliminate a lot of words
 
-# How to find the most overlapping words among all possible words?
-# Ans:  We need to take care of every position (here 0-5)and every position can have char from 'a-z'(0-25).
-# Means how many char matches at same position(index) among all the words.
-# for this we can make a matrix (position * char_index). char index for 'c' = ord(c) - ord("a") => this will bring in range of '0' to 25.
-# Now traverse each word and for every char just keep on adding '+1' for that cell (i, char_index).
+How to find the most overlapping words among all possible words?
+Ans:  We need to take care of every position (here 0-5)and every position can have char from 'a-z'(0-25).
+Means how many char matches at same position(index) among all the words.
+for this we can make a matrix (position * char_index). char index for 'c' = ord(c) - ord("a") => this will bring in range of '0' to 25.
+Now traverse each word and for every char just keep on adding '+1' for that cell (i, char_index).
 
-# At last again traverse each word and calculate the score same way we were doing '+1'.
-# The char having maximum score will be our most_overlapping character.
+At last again traverse each word and calculate the score same way we were doing '+1'.
+The char having maximum score will be our most_overlapping character.
 
-# 2) if most_overlaping is secret word then simply return 
-# 3) Else find the possible secret words.(Explained above).
+2) if most_overlaping is secret word then simply return 
+3) Else find the possible secret words.(Explained above).
+
+# time: O(w*l), w: no of words, l: length of each word
+# time: O(26*6)
+"""
 
 class Solution(object):
     def findSecretWord(self, wordlist, master):
@@ -217,9 +222,3 @@ public:
 };
 """
 
-# Method 2: Taking the random word (Above one is easier and good for interview)
-# Other Approaches are hard to understand and explain in interview.
-# https://leetcode.com/problems/guess-the-word/solutions/556075/how-to-explain-to-interviewer-843-guess-the-word/
-
-
-# Other way , do by this if you understand othwerwise leave.

0bb257ecbf7da0c551106a3d4789ac248c8e6fa6
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-06-11 01:05:04 +0530

    Update 2543. Check if Point Is Reachable.py

--- Maths and Geometry/2543. Check if Point Is Reachable.py
+++ Maths and Geometry/2543. Check if Point Is Reachable.py
@@ -1,18 +1,20 @@
 # Intution:
-# 1) If we move from (targetX, targetY) to the points (targetX, targetY - targetX) or 
-# (targetX - targetY, targetY), the GCD of the pair remains the same.
-# 2) If we move from (targetX, targetY) to the points (2 * targetX, targetY) or (targetX, 2 * targetY), 
-# the GCD of the pair remains same or gets doubled.
-# 3) GCD of (1, 1) is 1. From here it can either remain same or get multiplied by 2 each time. 
-# Therefore, from the above observations, point (1, 1) can move to point (targetX, targetY) 
-# if and only if gcd of (targetX, targetY) is a power of 2.
+"""
+1) If we move from (targetX, targetY) to the points (targetX, targetY - targetX) or 
+(targetX - targetY, targetY), the GCD of the pair remains the same.
+2) If we move from (targetX, targetY) to the points (2 * targetX, targetY) or (targetX, 2 * targetY), 
+the GCD of the pair remains same or gets doubled.
+3) GCD of (1, 1) is 1. From here it can either remain same or get multiplied by 2 each time. 
+Therefore, from the above observations, point (1, 1) can move to point (targetX, targetY) 
+if and only if gcd of (targetX, targetY) is a power of 2.
 
-# Approach:
+Approach:
 1) Find gcd of targetX and targetY
 2) Check if gcd is a power of 2. If it is, return True else return False.
 
-# Time complexity: O(gcd) = O(log(min(targetX, targetY)))
-# Space complexity: O(gcd)
+Time complexity: O(gcd) = O(log(min(targetX, targetY)))
+Space complexity: O(gcd)
+"""
 
 class Solution:
     def isReachable(self, targetX: int, targetY: int) -> bool:
@@ -39,12 +41,6 @@ using namespace std;
 
 class Solution {
 public:
-    /*
-    Explanation:
-    - Compute the GCD of `targetX` and `targetY`.
-    - If `g` is a power of 2, then `g & (g - 1) == 0` holds true.
-    - This ensures that the target can be reached.
-    */
     bool isReachable(int targetX, int targetY) {
         int g = gcd(targetX, targetY);
         return (g & (g - 1)) == 0; // Check if `g` is a power of 2

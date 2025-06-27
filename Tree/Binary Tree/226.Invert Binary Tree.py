8e1c1634271c46e3f75ed4916678a7fbefc05868
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-06-27 22:40:51 +0530

    Update 226.Invert Binary Tree.py

--- Tree/Binary Tree/226.Invert Binary Tree.py
+++ Tree/Binary Tree/226.Invert Binary Tree.py
@@ -1,7 +1,7 @@
+# method 1:
 # just change the pointer for each node going top- bottom.
 # Bottom - up will also work.
 
-# method 1:
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
@@ -10,7 +10,8 @@ class Solution:
         self.invertTree(root.left)
         self.invertTree(root.right)
         return root
-    
+
+
 # method 2:
 # converting the above one into iterative using 'BFS'
 class Solution:
@@ -26,52 +27,3 @@ class Solution:
             if curr.right:
                 q.append(curr.right)
         return root
-
-
-# Java
-"""
-// method 1:
-class Solution {
-    public TreeNode invertTree(TreeNode root) {
-        if (root == null) {
-            return null;
-        }
-        TreeNode temp = root.left;
-        root.left = root.right;
-        root.right = temp;
-        invertTree(root.left);
-        invertTree(root.right);
-        return root;
-    }
-}
-
-// method 2:
-
-class Solution {
-    public TreeNode invertTree(TreeNode root) {
-        if (root == null) {
-            return root;
-        }
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-
-        while (!queue.isEmpty()) {
-            TreeNode current = queue.poll();
-            // Swap the left and right children
-            TreeNode temp = current.left;
-            current.left = current.right;
-            current.right = temp;
-
-            // Add children to the queue if they exist
-            if (current.left != null) {
-                queue.offer(current.left);
-            }
-            if (current.right != null) {
-                queue.offer(current.right);
-            }
-        }
-        return root;
-    }
-    
-"""

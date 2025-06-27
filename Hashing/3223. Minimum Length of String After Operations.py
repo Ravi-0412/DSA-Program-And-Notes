d4a0bfac8d48b8aa5b72c3a5258ea43af46e207d
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-06-27 22:35:13 +0530

    Update 3223. Minimum Length of String After Operations.py

--- Hashing/3223. Minimum Length of String After Operations.py
+++ Hashing/3223. Minimum Length of String After Operations.py
@@ -1,4 +1,4 @@
-# logic:
+# Method 1: 
 """
 suppose any char has freq 'v' then all will get cancelled except either one or two character.
 If 'v' is even then : 2 char will be remaining else only '1' char
@@ -14,27 +14,3 @@ class Solution:
         for val in freq.values():
             ans += 2 if val % 2 == 0 else 1
         return ans
-
-  # Java
-  """
-  import java.util.HashMap;
-
-class Solution {
-    public int minimumLength(String s) {
-        HashMap<Character, Integer> freq = new HashMap<>();
-        int ans = 0;
-        
-        // Count frequency of each character in the string
-        for (char ch : s.toCharArray()) {
-            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
-        }
-        
-        // Calculate the result based on frequencies
-        for (int val : freq.values()) {
-            ans += (val % 2 == 0) ? 2 : 1;
-        }
-        
-        return ans;
-    }
-}
-  """

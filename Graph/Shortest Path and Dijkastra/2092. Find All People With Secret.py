88c9b3ca45c27e97a4a8c893d46ce32df871dc9a
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2025-07-17 20:04:32 +0530

    Update 2092. Find All People With Secret.py

--- Graph/Shortest Path and Dijkastra/2092. Find All People With Secret.py
+++ Graph/Shortest Path and Dijkastra/2092. Find All People With Secret.py
@@ -1,47 +1,4 @@
-# Logic: Person who will know the secret will tell all his connected persons.
-# Person who come to know the secret at less time will tell more person.
-
-# Hmko max person ko include karna h answer me isliye hm chahenge ki koi person
-# jitna kam time me secret jane taki aage wale ko kam time me bta sake ans so on & finally mera ans me maximum log 
-# include ho jayega.
-
-# For this we need to pop person according to lesser time.
-# we want as lesser time for every person to include max person in our ans. => minHeap
-
-# Note: But when any person will come the secret that may not be the least time for that person,
-# that person can know the secret at more lesser time by other person.
-# Just same as Dijkastra. mark visited only after poping.
-
-# Note: So when you will mark any person visited, when you will see them for 1st time like bfs then,
-# You will get wrong ans. You will get less no of people than expected. 
-
-# Time: Same as Dijkastra
-
-class Solution:
-    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
-        graph = collections.defaultdict(list)  # list of person with time with whome he can share the secret.
-        for x, y, t in meetings:
-            graph[x].append((y, t))
-            graph[y].append((x, t))
-        
-        minHeap = []
-        heapq.heappush(minHeap, (0, 0))    # (time_at_which_person_knows_secret, person
-                    # this person can tell secret to other person having >= this time.
-        heapq.heappush(minHeap, (0, firstPerson))
-        knows = set()   # will only add once that person will share secret with all the possible person
-        while minHeap :
-            time, person = heapq.heappop(minHeap)
-            if person in knows:
-                continue
-            # This person knows the secret and now share with other person
-            knows.add(person)
-            for p, t in graph[person]:
-                if p not in knows and t >= time:   # because of time condition a lot of meeting will be get discarded
-                    # will only tell if timing 't' will be >= time
-                    heapq.heappush(minHeap, (t, p))
-        return list(knows)
-
-
+# My mistakes: 
 # My mistakes
 # 1) Sorting acc to time and checking all the meetings and adding in ans according to them.
 # Error why? 
@@ -95,3 +52,51 @@ class Solution:
 
 
 # 3) Marking visited when we will see any person for first time.
+
+
+# Method 1: 
+
+# Logic: Person who will know the secret will tell all his connected persons.
+# Person who come to know the secret at less time will tell more person.
+
+# Hmko max person ko include karna h answer me isliye hm chahenge ki koi person
+# jitna kam time me secret jane taki aage wale ko kam time me bta sake ans so on & finally mera ans me maximum log 
+# include ho jayega.
+
+# For this we need to pop person according to lesser time.
+# we want as lesser time for every person to include max person in our ans. => minHeap
+
+# Note: But when any person will come the secret that may not be the least time for that person,
+# that person can know the secret at more lesser time by other person.
+# Just same as Dijkastra. mark visited only after poping.
+
+# Note: So when you will mark any person visited, when you will see them for 1st time like bfs then,
+# You will get wrong ans. You will get less no of people than expected. 
+
+# Time: Same as Dijkastra
+
+class Solution:
+    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+        graph = collections.defaultdict(list)  # list of person with time with whome he can share the secret.
+        for x, y, t in meetings:
+            graph[x].append((y, t))
+            graph[y].append((x, t))
+        
+        minHeap = []
+        heapq.heappush(minHeap, (0, 0))    # (time_at_which_person_knows_secret, person
+                    # this person can tell secret to other person having >= this time.
+        heapq.heappush(minHeap, (0, firstPerson))
+        knows = set()   # will only add once that person will share secret with all the possible person
+        while minHeap :
+            time, person = heapq.heappop(minHeap)
+            if person in knows:
+                continue
+            # This person knows the secret and now share with other person
+            knows.add(person)
+            for p, t in graph[person]:
+                if p not in knows and t >= time:   # because of time condition a lot of meeting will be get discarded
+                    # will only tell if timing 't' will be >= time
+                    heapq.heappush(minHeap, (t, p))
+        return list(knows)
+
+

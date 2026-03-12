d78cfc4312a6d618b123eae760874887e9c7ed50
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2026-03-12 10:30:08 +0530

    Update 905. Sort Array By Parity.py

--- Two Pointer/905. Sort Array By Parity.py
+++ Two Pointer/905. Sort Array By Parity.py
@@ -1,16 +1,16 @@
 # # 1st method(myself) : time= o(n) ,space= o(n)
-# class Solution:
-#     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-#         n= len(nums)
-#         even_list= []
-#         odd_list=  []
-#         for val in nums:
-#             if(val%2==0):
-#                 even_list.append(val)
-#             else:
-#                 odd_list.append(val)
-#         even_list=even_list+odd_list
-#         return even_list
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        n= len(nums)
+        even_list= []
+        odd_list=  []
+        for val in nums:
+            if(val%2==0):
+                even_list.append(val)
+            else:
+                odd_list.append(val)
+        even_list=even_list+odd_list
+        return even_list
 
 
 # # 2nd method: very much concise and elegant way of 1st method
@@ -45,30 +45,31 @@ class Solution:
             
 
 # just another way of writing the above logic
-# class Solution:
+# Better one 
+"""
+Move start forward until it finds an odd number
+Move end backward until it finds an even number
+Then swap them
+"""
+class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        n= len(nums)
-        start, end= 0, n-1
-        # start will denote the curr index and from 'end' index all the elements will be odd only
-        # in this case you always have to incr start by '1' as after each iteration one ele from start is getting placed at proper position
-        while(start <end):
-            if(nums[start]%2!= 0): # if odd find the position of 1st even index from last
-                while start <end and nums[end]%2 != 0 :
-                    end-= 1
-                nums[start], nums[end]= nums[end], nums[start]
-                start+= 1
-            else: 
-                start+= 1   # in this case don't decr 'end '
+        n = len(nums)
+        start, end = 0, n - 1
 
-            # since we have to incr start always so we can write start+= 1 outside the if-else statement like: 
+        while start < end:
             
-            # while(start <end):
-            #     if(nums[start]%2!= 0): # if odd swap ele at start and end index
-            #       while start <end and nums[end]%2 != 0 :
-            #         end-= 1
-            #     nums[start], nums[end]= nums[end], nums[start]
-            # start+= 1   # in this case don't decr 'end '
-        return nums     
+            # move start forward while element is even
+            while start < end and nums[start] % 2 == 0:
+                start += 1
+            
+            # move end backward while element is odd
+            while start < end and nums[end] % 2 == 1:
+                end -= 1
+            
+            # swap odd at start with even at end
+            nums[start], nums[end] = nums[end], nums[start]
+
+        return nums 
         
         
         

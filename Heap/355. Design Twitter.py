1c84d577e3b47cea2525a20b71e4a7105a09faf8
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2026-03-25 00:24:18 +0530

    Update 355. Design Twitter.py

--- Heap/355. Design Twitter.py
+++ Heap/355. Design Twitter.py
@@ -1,38 +1,40 @@
 # Method 1: 
 
-# first  main tasks to identify the data structure for each function with minimal time complexity.
+"""
+First  main tasks to identify the data structure for each function with minimal time complexity.
 
-# 1) for 'follow' and 'unfollow' 
-# we can make a hashmap wrt each user and store all the people whom the user follow in a list
-# but in case if he unfollows then we will have to remove that user from the list of 'following person' and list will take O(n)
-# so list will not work .
-# then only think come into mind that can do the adding and removing thing in O(1) is set
-# so hashset will work fine for this case
+1) for 'follow' and 'unfollow' 
+we can make a hashmap wrt each user and store all the people whom the user follow in a list
+but in case if he unfollows then we will have to remove that user from the list of 'following person' and list will take O(n)
+so list will not work .
+then only think come into mind that can do the adding and removing thing in O(1) is set
+so hashset will work fine for this case
 
-# Note: always remember when you have to add or delete(not from a fixed position) then 'set' should come into mind.
+Note: always remember when you have to add or delete(not from a fixed position) then 'set' should come into mind.
 
-# 2) postTweet: we can take a hashmap and store the tweets done by user in a list, 
-# the most recent tweets done by user will be at the end of the list.
-# But to get the most recent feed we have to store the tweet done by a user with time also.
-# Since we can use only minHeap so we will reduce the time each time by '1' instead of increment.
+2) postTweet: we can take a hashmap and store the tweets done by user in a list, 
+the most recent tweets done by user will be at the end of the list.
+But to get the most recent feed we have to store the tweet done by a user with time also.
+Since we can use only minHeap so we will reduce the time each time by '1' instead of increment.
 
-# 3)getNewsFeed:
-# wo user jisko bhi follow kar rha, unlog ka 1o most recent tweet chahiye.
-# for getting the most recent 10 tweets, we have to find the 10 most recent 
-# from all the lists of tweets done by person whom user follow.
-# Before adding into MinHeap, most recent tweet will be at last only.
+3)getNewsFeed:
+wo user jisko bhi follow kar rha, unlog ka 1o most recent tweet chahiye.
+for getting the most recent 10 tweets, we have to find the 10 most recent 
+from all the lists of tweets done by person whom user follow.
+Before adding into MinHeap, most recent tweet will be at last only.
 
-# Vvi: Now the problem reduces to "merge k sorted array" for 'getNewsFeed'.
-# Most recent tweet will be at the top for any person because we are storing in minHeap.
+Vvi: Now the problem reduces to "merge k sorted array" for 'getNewsFeed'.
+Most recent tweet will be at the top for any person because we are storing in minHeap.
 
-# Why using minHeap: Just to avoid adding '-ve' value in case of maxHeap. can do by maxHeap also.
+Why using minHeap: Just to avoid adding '-ve' value in case of maxHeap. can do by maxHeap also.
 
-# just compare the last tweet time of each person they follow
-# and most efficient way to do this is add all the recent tweet done by all the person they follow into a minHeap.
-# After that pop from minHeap to get the most recent one add that to the ans.
+just compare the last tweet time of each person they follow
+and most efficient way to do this is add all the recent tweet done by all the person they follow into a minHeap.
+After that pop from minHeap to get the most recent one add that to the ans.
 
-# for getNewsfeed ,time: O(10*logk)   # k : no of person a user is following, 10 times we have to heapify.
-# And for remaining operation time: O(1).
+for getNewsfeed ,time: O(10*logk)   # k : no of person a user is following, 10 times we have to heapify.
+And for remaining operation time: O(1).
+"""
 
 import heapq
 from collections import defaultdict

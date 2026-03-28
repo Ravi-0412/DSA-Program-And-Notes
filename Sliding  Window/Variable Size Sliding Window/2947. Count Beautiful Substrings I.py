1a1eae60bf4aab114c226a87f118fb58c147c7c2
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2026-03-28 12:32:24 +0530

    Update 2947. Count Beautiful Substrings I.py

--- Sliding  Window/Variable Size Sliding Window/2947. Count Beautiful Substrings I.py
+++ Sliding  Window/Variable Size Sliding Window/2947. Count Beautiful Substrings I.py
@@ -1,41 +1,32 @@
 # Just extension  of "525. Contiguous Array"
 
-# Here we also need to take care of individual count of 'vowel' and consonant.
-# FOr this also we will store index in a list w.r.t to count. [cnt: [indexes]]
-# And we will check for each index if count of both is divisible by 'k' or not.
+"""
+Logic: For each substring, we count how many vowels and consonants it contains.
+Check Conditions: vowels == consonants and (vowels * consonants) % k == 0
 
-# For finding count in O(1), we will use prefix sum.
-
-# Time : O(n^2)
+Time : O(n^2)
+"""
 
 class Solution:
     def beautifulSubstrings(self, s: str, k: int) -> int:
         n = len(s)
-        prefix_v = [0] *(n + 1)
-        prefix_c = [0]*(n + 1)
-        for i in range(1, n + 1):
-            if s[i-1] in "aeiou":
-                prefix_v[i] = 1 + prefix_v[i-1]
-            else:
-                prefix_v[i] = prefix_v[i-1]
-            if s[i -1] not in "aeiou":
-                prefix_c[i] = 1 + prefix_c[i-1]
-            else:
-                prefix_c[i] = prefix_c[i-1]
-    
-        freq= collections.defaultdict(list)
-        freq[0] = [-1]
-        cnt = 0
         ans = 0
+        vowels_set = set("aeiou")
+        
         for i in range(n):
-            if s[i] in "aeiou":
-                cnt += 1
-            else:
-                cnt -= 1
-            if cnt in freq:
-                l = len(freq)
-                for j in freq[cnt]:
-                    if ((prefix_v[i + 1] - prefix_v[j + 1]) * (prefix_c[i + 1] - prefix_c[j + 1])) % k == 0:
-                        ans += 1     
-            freq[cnt].append(i)
+            v = 0
+            c = 0
+            for j in range(i, n):
+                if s[j] in vowels_set:
+                    v += 1
+                else:
+                    c += 1
+                # check if beautiful subarray
+                if v == c and (v * c) % k == 0:
+                    ans += 1
         return ans
+
+# Method 2:
+# Optimised one : Do it later by understanding properly
+"""
+"""

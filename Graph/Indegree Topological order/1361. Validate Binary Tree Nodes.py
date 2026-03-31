660463acba90c60f532d7423e5e0bea2eabc1398
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2026-03-31 17:03:39 +0530

    Update 1361. Validate Binary Tree Nodes.py

--- Graph/Indegree Topological order/1361. Validate Binary Tree Nodes.py
+++ Graph/Indegree Topological order/1361. Validate Binary Tree Nodes.py
@@ -1,21 +1,24 @@
 # Method 1: 
 
-# Explanation:
-# a)  binary tree will have exactly one root node i.e only one node will have indegree == 0 i.e root.
-# b ) from root, all nodes can be traversed exactly once.
-# c ) all nodes will have indegree exactly equal to 1.
-# d ) tree should have exactly n-1 edge.
-# Space complexity: O(n)
-# time complexity: O(n+e)
+"""
+Explanation:
+a)  binary tree will have exactly one root node i.e only one node will have indegree == 0 i.e root.
+b ) from root, all nodes can be traversed exactly once.
+c ) all nodes will have indegree exactly equal to 1.
+d ) tree should have exactly n-1 edge.
 
-# Note: If you will check only these two conditions
-# a)  There should be one node with in-degree = 0
-# b) All the other nodes should have in-degree =1
+Note: If you will check only these two conditions , will work but only in single component. Won't work in case of more than one component.
+a)  There should be one node with in-degree = 0
+b) All the other nodes should have in-degree =1
 
-# Won't work in case of more than one component.
 
-# Note: Checking for no cycle and single component will wrong ans because
-# in this one node can more than two children.
+
+Note: Checking for no cycle and single component will wrong ans because
+in this one node can more than two children.
+
+Space complexity: O(n)
+time complexity: O(n+e)
+"""
 
 
 # method 2: 

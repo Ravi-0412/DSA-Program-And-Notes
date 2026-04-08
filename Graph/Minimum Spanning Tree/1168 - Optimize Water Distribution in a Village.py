747e447fd2eff5becde0031cb2c9330f762127d9
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2026-04-08 23:21:07 +0530

    Update 1168 - Optimize Water Distribution in a Village.py

--- Graph/Minimum Spanning Tree/1168 - Optimize Water Distribution in a Village.py
+++ Graph/Minimum Spanning Tree/1168 - Optimize Water Distribution in a Village.py
@@ -1,16 +1,20 @@
 # submitted on coding ninja
 
-# logic: just we have to connect all the village with water.
-# for this there are two ways: 1) either buliding wells at the village or 2) connecting by pipes through already connected village.
+"""
+logic: just we have to connect all the village with water.
+for this there are two ways: 1) either buliding wells at the village or 2) connecting by pipes through already connected village.
 
-# since we have to connect all village with minimum cost then minimum spanning tree should come into mind.
+since we have to connect all village with minimum cost then minimum spanning tree should come into mind.
 
-# how to do?
-# Ans: First we have to build well at any of the village then only we can connect other villages through it 
-# otherwise we will build the wells at other villages also.
+how to do?
+Ans: First we have to build well at any of the village then only we can connect other villages through it 
+otherwise we will build the wells at other villages also.
 
-# Note: We don't know digging well at which village will give optimal ans, we can get starting from any village.
-# so put cost of all building wells at all villages into heap and then after that totally same as mst.
+Note: We don't know digging well at which village will give optimal ans, we can get starting from any village.
+so put cost of all building wells at all villages into heap and then after that totally same as mst.
+
+Time : O((N + K) * logN), N: edges, K : wells
+"""
 
 import collections
 import heapq
@@ -20,9 +24,8 @@ def supplyWater(n, k, wells, pipes):
 	for s,d,c in pipes:
 		adj[s].append((d, c))
 		adj[d].append((s, c))
-	minHeap= []
-	for i in range(n):
-		heapq.heappush(minHeap, (wells[i], i+1))
+	minHeap = [(wells[i], i + 1) for i in range(n)]
+	heapq.heapify(minHeap)
 	visited= set()
 	minCost= 0
 	while minHeap:

bb90199546bae9b12fd156a79e6d4a586adf4813
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2026-04-16 13:22:46 +0530

    Update 968. Binary Tree Cameras.py

--- Tree/Binary Tree/968. Binary Tree Cameras.py
+++ Tree/Binary Tree/968. Binary Tree Cameras.py
@@ -7,9 +7,10 @@ ii) Place a camera on the parent node of the leaf node.
 
 But placing on parent will be reduce the no of cameras because then more node can be monitored.
 
-0: This node needs a camera
-1: This node has a camera
-2: This node is already monitored using neighbour camera.
+We define three states for any node:
+    State 0: "I am UNCOVERED." (I have no camera and no one is watching me)
+    State 1: "I HAVE A CAMERA." (I am a camera; I cover myself, my kids, and my parent)
+    State 2: "I AM COVERED." (I don't have a camera, but one of my kids is watching me)
 
 Going Bottom up, for each node: 
 i)   If any child needs a camera (i.e. child returned 0) → this node must have a camera
@@ -21,31 +22,44 @@ time: O(n)
 
 class Solution:
     def minCameraCover(self, root: Optional[TreeNode]) -> int:
-        self.cameras = 0  # Counter to track the number of cameras placed
-
+        self.cameras = 0
+        
         def dfs(node):
+            # Base Case: If we reach a null child (empty space)
+            # We treat it as "Covered" (State 2) so it doesn't force us to 
+            # put cameras on the leaves unnecessarily.
             if not node:
-                return 2  # Null nodes are considered covered
-
-            left = dfs(node.left)   
-            right = dfs(node.right) 
-
-            # If any child needs a camera, place a camera at this node
-            if left == 0 or right == 0:
+                return 2
+            
+            # Go all the way down to the bottom first (Post-order traversal)
+            left_child_state = dfs(node.left)
+            right_child_state = dfs(node.right)
+            
+            # --- DECISION TIME ---
+            
+            # CASE 1: One of my children is "UNCOVERED" (State 0)
+            # If a child is naked, I MUST put a camera here to save them.
+            if left_child_state == 0 or right_child_state == 0:
                 self.cameras += 1
-                return 1  # Node has a camera
+                return 1 # Now I am a camera
+            
+            # CASE 2: One of my children HAS A CAMERA (State 1)
+            # If a child is a camera, they are looking up at me. I am safe!
+            if left_child_state == 1 or right_child_state == 1:
+                return 2 # Now I am covered
+            
+            # CASE 3: Both children are "COVERED" (State 2) but have NO camera
+            # My kids are safe, but they aren't looking at me. 
+            # I am now "UNCOVERED" (State 0). I'll wait for my parent to save me.
+            return 0
 
-            # If any child has a camera, this node is covered
-            if left == 1 or right == 1:
-                return 2  # Node is covered
-
-            # If both children are covered but do not have cameras, this node needs a camera
-            return 0  # Node needs a camera
-
-        # After DFS, if root is still not covered, we need to place a camera at the root
+        # After the DFS finishes, check the very top (the Root).
+        # If the root says "I am UNCOVERED," there's no parent left to save it.
+        # We must put a camera on the root itself.
+        # Needed this check separately because some node is dependent on it's parent node also to cover it ("I won't cover myself; I'll wait for my parent to cover me.) 
         if dfs(root) == 0:
             self.cameras += 1
-
+            
         return self.cameras
 
 # Java Code 

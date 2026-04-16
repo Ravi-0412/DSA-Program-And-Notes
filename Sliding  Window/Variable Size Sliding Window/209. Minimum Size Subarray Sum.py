863172e0eec1865c57c0c52d72b969cfc77b1f52
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2026-04-16 20:52:32 +0530

    Update 209. Minimum Size Subarray Sum.py

--- Sliding  Window/Variable Size Sliding Window/209. Minimum Size Subarray Sum.py
+++ Sliding  Window/Variable Size Sliding Window/209. Minimum Size Subarray Sum.py
@@ -1,37 +1,39 @@
 # Method 1: 
 
-# Note: Appplied same logic as "76. Minimum Window Substring".
-# i.e keep chekcing if you have find any ans and then try to shrink the subarray and keep updating the ans.
+"""
+Note: Appplied same logic as "76. Minimum Window Substring".
+i.e keep chekcing if you have find any ans and then try to shrink the subarray and keep updating the ans.
 
-# Here all ele is +ve and we are asked to find the minimum length so if curSum becomes >= target then we will have to remove ele from left
-# Because adding more ele will lead to more curSum and we will get the wrong ans(i.e bigger value than expected).
+Here all ele is +ve and we are asked to find the minimum length so if curSum becomes >= target then we will have to remove ele from left
+Because adding more ele will lead to more curSum and we will get the wrong ans(i.e bigger value than expected).
 
-# Note vvi: Whenever asked to find the minimum subarray length and that we can get by shrinking the window 
-# then, always we will update the ans while shrinking the window till condition is valid i.e inside 2nd while loop.
+Note vvi: Whenever asked to find the minimum subarray length and that we can get by shrinking the window 
+then, always we will update the ans while shrinking the window till condition is valid i.e inside 2nd while loop.
 
-# vvi: i.e condition bhi >= ka ho and minimum bhi chahiye then yhi logic use karenge.
-# shrink the window in valid conition to get the minimum ans (keep updating the ans while whrinking the window).
+vvi: i.e condition bhi >= ka ho and minimum bhi chahiye then yhi logic use karenge.
+shrink the window in valid conition to get the minimum ans (keep updating the ans while whrinking the window).
 
-# Note : We can also check if there exist any possible subarray or not.
-# If sum(nums) < target then no subarray possible.
+Note : We can also check if there exist any possible subarray or not.
+If sum(nums) < target then no subarray possible.
 
-# How to solve?
-# Ans: Just keep on adding the cur ele to 'sum' and after adding keep on reducing the window size from left
-# till our window is valid because we need the 'minimum' length of subarray.
+How to solve?
+Ans: Just keep on adding the cur ele to 'sum' and after adding keep on reducing the window size from left
+till our window is valid because we need the 'minimum' length of subarray.
 
-# Just similar logic as "76. Minimum Window Substring".
+Just similar logic as "76. Minimum Window Substring".
+"""
 
 """
-# Note: it won't work if there is "-ve" number also since inner while loop can break before finding the 
-# shortest subarray after adding the curr ele.
+Note: it won't work if there is "-ve" number also since inner while loop can break before finding the 
+shortest subarray after adding the curr ele.
 
-# e.g: [84,-37,32,40,95], target= 167.
-# it will give output= 5 but it should be equal= 3.
-# Reason: '95' ko add karne ke bad sum >= 167 ho jayega and jb hm window ko shrink karenge after removing
-# '87' then sum < 167 ho jayega and loop break ho jayega. 
-# But agar '-37' ko bhi remove kar de tb bhi mera sum > 167 rhega but ye case consider hi nhi hoga.
-# e.g: [3,-2,5], k= 4. 
-# output= 3 but ans should = 1.
+e.g: [84,-37,32,40,95], target= 167.
+it will give output= 5 but it should be equal= 3.
+Reason: '95' ko add karne ke bad sum >= 167 ho jayega and jb hm window ko shrink karenge after removing
+'87' then sum < 167 ho jayega and loop break ho jayega. 
+But agar '-37' ko bhi remove kar de tb bhi mera sum > 167 rhega but ye case consider hi nhi hoga.
+e.g: [3,-2,5], k= 4. 
+output= 3 but ans should = 1.
 """
 
 class Solution:

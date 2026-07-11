d0e2869369009b305c16723c3285131a25d55eb5
Ravi Raushan <44740228+Ravi-0412@users.noreply.github.com>
2026-07-11 14:56:51 +0530

    Update 546. Remove Boxes.py

--- Dynamic Programming/Matrix_Chain_Multiplication/546. Remove Boxes.py
+++ Dynamic Programming/Matrix_Chain_Multiplication/546. Remove Boxes.py
@@ -1,25 +1,27 @@
-# Note vvi: if n = a + b + c + .... then n^2 > a^2 + b^2 + c^2 + ...
-# So it is better to remove the maximum possible ele of same color to get the maximum points.
+"""
+Note vvi: if n = a + b + c + .... then n^2 > a^2 + b^2 + c^2 + ...
+So it is better to remove the maximum possible ele of same color to get the maximum points.
 
-# Recursive way TLE
+Recursive way TLE
 
-# Note : Let dp(l, r, k) denote the maximum points we can get in boxes[l..r] if we have extra k boxes which is the same color with boxes[l] in the left side.
-# For example: boxes = [3, 3, 1, 3, 3]
-# The dp(l=3, r=4, k=2) is the maximum points we can get in boxes[3..4] if we have extra 2 boxes the same color with boxes[3] in the left side, 
-# it's the same as we find the maximum points in boxes = [3, 3, 3, 3] .
+Note : Let dp(l, r, k) denote the maximum points we can get in boxes[l..r] if we have extra k boxes which is the same color with boxes[l] in the left side.
+For example: boxes = [3, 3, 1, 3, 3]
+The dp(l=3, r=4, k=2) is the maximum points we can get in boxes[3..4] if we have extra 2 boxes the same color with boxes[3] in the left side, 
+it's the same as we find the maximum points in boxes = [3, 3, 3, 3] .
 
-# Since (a+b)^2 > a^2 + b^2, where a > 0, b > 0, so it's better to greedy to remove all contiguous boxes of the same color, instead of split them.
-# So we increase both l and k while boxes[l+1] == boxes[l].
+Since (a+b)^2 > a^2 + b^2, where a > 0, b > 0, so it's better to greedy to remove all contiguous boxes of the same color, instead of split them.
+So we increase both l and k while boxes[l+1] == boxes[l].
 
-# Now, we have many options to consider:
+Now, we have many options to consider:
 
-# Option 1, remove all boxes which has the same with boxes[l], total points we can get is dp(l+1, r, 0) + (k+1)*(k+1) (k left boxes and the lth box have the same color)
-# Other options, we try to merge non-contiguous boxes of the same color together, by:
-# Find the index j, where l+1 <= j <= r so that boxes[j] == boxes[l].
-# Total points we can get is dp(j, r, k+1) + dp(l+1, j-1, 0)
+Option 1, remove all boxes which has the same with boxes[l], total points we can get is dp(l+1, r, 0) + (k+1)*(k+1) (k left boxes and the lth box have the same color)
+Other options, we try to merge non-contiguous boxes of the same color together, by:
+Find the index j, where l+1 <= j <= r so that boxes[j] == boxes[l].
+Total points we can get is dp(j, r, k+1) + dp(l+1, j-1, 0)
 
-# https://leetcode.com/problems/remove-boxes/solutions/1402561/c-java-python-top-down-dp-clear-explanation-with-picture-clean-concise/
-# https://www.youtube.com/watch?v=ncW3zm_J3qY
+https://leetcode.com/problems/remove-boxes/solutions/1402561/c-java-python-top-down-dp-clear-explanation-with-picture-clean-concise/
+https://www.youtube.com/watch?v=ncW3zm_J3qY
+"""
 
 class Solution:
     def removeBoxes(self, boxes: List[int]) -> int:
